0cc129cda94e833f8c9786ff783e84cca7ba1b60
Inevitabilis <70079177+Inevitabilis@users.noreply.github.com>
2024-07-11 19:14:25 +0300

    Optimized

--- src/SelectScreenScenes.cs
+++ src/SelectScreenScenes.cs
@@ -15,67 +15,9 @@ namespace VoidTemplate;
 
 internal static class SelectScreenScenes
 {
-    private const string slugbasedivider = "_SlugBaseSaveData_";
-    public static ConditionalWeakTable<SlugcatSelectMenu.SaveGameData, List<SaveStateMiner.Result>> associatedSaveData = new();
-    private static bool ParseSlugbaseData<T>(this string str, out T value)
-    {
-        value = default(T);
-        string preparedString = Encoding.UTF8.GetString(Convert.FromBase64String(str));
-        try
-        {
-            value = JsonConvert.DeserializeObject<T>(preparedString);
-            return true;
-        }
-        catch (Exception ex)
-        {
-            Plugin.logger.LogError($"Failed to parse slugbase data of type {nameof(T)}");
-            return false;
-        }
-
-        
-    }
-    private static bool TryGetDataFromMine<T>(this List<SaveStateMiner.Result> list, string key, out T value)
-    {
-        value = default;
-        foreach (var item in list)
-        {
-            if (item.name == key && item.data.ParseSlugbaseData<T>(out T val))
-            {
-                value = val;
-                Plugin.logger.LogMessage($"TRY GET DATA found the matching data for key {key}: {val}");
-                return true;
-            }
-        }
-        return false;
-    }
     public static void Hook()
     {
         On.Menu.MenuScene.BuildScene += CustomSelectScene;
-        On.Menu.SlugcatSelectMenu.MineForSaveData += SlugcatSelectMenu_MineForSaveData;
-    }
-
-    private static SlugcatSelectMenu.SaveGameData SlugcatSelectMenu_MineForSaveData(On.Menu.SlugcatSelectMenu.orig_MineForSaveData orig, ProcessManager manager, SlugcatStats.Name slugcat)
-    {
-        var result = orig(manager, slugcat);
-        if(slugcat != StaticStuff.TheVoid) return result;
-        string[] progLinesFromMemory = manager.rainWorld.progression.GetProgLinesFromMemory();
-        for (int i = 0; i < progLinesFromMemory.Length; i++)
-        {
-            string[] array = Regex.Split(progLinesFromMemory[i], "<progDivB>");
-            if (array.Length == 2 && array[0] == "SAVE STATE" && BackwardsCompatibilityRemix.ParseSaveNumber(array[1]) == TheVoid)
-            {
-                List<SaveStateMiner.Target> list =
-                [
-                    new SaveStateMiner.Target(SaveManager.endingDone, slugbasedivider, "<mwA>", 1024),
-                    new SaveStateMiner.Target(SaveManager.VisitedFP6times, slugbasedivider, "<mwA>", 1024)
-                ];
-                List<SaveStateMiner.Result> results = SaveStateMiner.Mine(manager.rainWorld, array[1], list);
-                Plugin.logger.LogMessage(array[1]);
-                Plugin.logger.LogMessage($"miner found {results.Count} results");
-                associatedSaveData.Add(result, results);
-            }
-        }
-        return result;
     }
 
     private static void CustomSelectScene(On.Menu.MenuScene.orig_BuildScene orig, Menu.MenuScene self)
@@ -93,13 +35,6 @@ internal static class SelectScreenScenes
             SaveState save = RWCustom.Custom.rainWorld.progression.GetOrInitiateSaveState(StaticStuff.TheVoid, null, self.menu.manager.menuSetup, false);
             if (save.GetEndingEncountered()) self.sceneID = DeathSceneID;
             else if (save.GetVisitedPebblesSixTimes()) self.sceneID = SelectFPScene;
-            /*if (associatedSaveData.TryGetValue(page2.saveGameData, out var subdata))
-            {
-                Plugin.logger.LogMessage("found the associated data");
-                if (subdata.TryGetDataFromMine<bool>(SaveManager.endingDone, out bool ended) && ended) self.sceneID = Sub;
-                else if (subdata.TryGetDataFromMine<bool>(SaveManager.VisitedFP6times, out bool visited) && visited) self.sceneID = SelectFPScene;
-
-            }*/
         }
         orig(self);
     }

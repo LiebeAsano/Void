3e1a0184ae80096ef021e9eea8ac142b650145ec
Inevitabilis <ddd1020018@gmail.com>
2024-07-16 21:58:08 +0300

    SaveManagerRefactor

--- src/SaveManager.cs
+++ src/SaveManager.cs
@@ -7,20 +7,68 @@ public static class SaveManager
     private const string uniqueprefix = "VoidSlugcat";
     private const string teleportationDone = uniqueprefix + "TeleportationDone";
     private const string messageShown = uniqueprefix + "MessageShown";
-    private const string lastMeetCycles = uniqueprefix + "LastMeetCycles";
-    private const string pebblesPearlsEaten = uniqueprefix + "PebblesPearlsEaten";
 
     public const string endingDone = uniqueprefix + "EndingDone";
     private const string voidCatDead = uniqueprefix + "VoidCatDead";
 
+    public static bool GetTeleportationDone(this SaveState save) => save.miscWorldSaveData.GetSlugBaseData().TryGet(teleportationDone, out bool done) && done;
+    public static void SetTeleportationDone(this SaveState save, bool value) => save.miscWorldSaveData.GetSlugBaseData().Set(teleportationDone, value);
+
+    public static bool GetMessageShown(this SaveState save) => save.miscWorldSaveData.GetSlugBaseData().TryGet(messageShown, out bool shown) && shown;
+    public static void SetMessageShown(this SaveState save, bool value) => save.miscWorldSaveData.GetSlugBaseData().Set(messageShown, value);
+
+
+    #region oracle data
+    private const string lastMeetCycles = uniqueprefix + "LastMeetCycles";
+    private const string pebblesPearlsEaten = uniqueprefix + "PebblesPearlsEaten";
+    private const string moonPearlsEaten = uniqueprefix + "MoonPearlsEaten";
+    private const string VisitedFP6times = uniqueprefix + "visitedFP6times";
+    #region last meet
+    public static int GetLastMeetCycles(this SaveState save)
+    {
+        var data = save.miscWorldSaveData.GetSlugBaseData();
+        if(!data.TryGet(lastMeetCycles, out int cycles))
+        {
+            cycles = 0;
+            data.Set(lastMeetCycles, 0);
+        }
+        return cycles;
+    }
+    public static void SetLastMeetCycles(this SaveState save, int cycles) => save.miscWorldSaveData.GetSlugBaseData().Set(lastMeetCycles, cycles);
+    #endregion
+    public static int GetPebblesPearlsEaten(this SaveState save)
+    {
+        var data = save.miscWorldSaveData.GetSlugBaseData();
+        if (!data.TryGet(pebblesPearlsEaten, out int eatenPearlsAmount))
+        {
+            eatenPearlsAmount = 0;
+            data.Set(pebblesPearlsEaten, 0);
+        }
+        return eatenPearlsAmount;
+    }
+    public static void SetPebblesPearlsEaten(this SaveState save, int amount) => save.miscWorldSaveData.GetSlugBaseData().Set(pebblesPearlsEaten, amount);
+
+    public static int GetMoonPearlsEaten(this SaveState save)
+    {
+        var data = save.miscWorldSaveData.GetSlugBaseData();
+        if (!data.TryGet(moonPearlsEaten, out int eatenPearlsAmount))
+        {
+            eatenPearlsAmount = 0;
+            data.Set(moonPearlsEaten, 0);
+        }
+        return eatenPearlsAmount;
+    }
+    public static void SetMoonPearlsEaten(this SaveState save, int amount) => save.miscWorldSaveData.GetSlugBaseData().Set(moonPearlsEaten, amount);
+    #endregion
+
+    public static bool GetVoidCatDead(this SaveState save) => save.miscWorldSaveData.GetSlugBaseData().TryGet(voidCatDead, out bool dead) && dead;
+    public static void SetVoidCatDead(this SaveState progression, bool value) => progression.miscWorldSaveData.GetSlugBaseData().Set(voidCatDead, value);
+
+    public static bool GetEndingEncountered(this SaveState save) => save.miscWorldSaveData.GetSlugBaseData().TryGet(endingDone, out bool done) && done;
+    public static void SetEndingEncountered(this SaveState save, bool value) => save.miscWorldSaveData.GetSlugBaseData().Set(endingDone, value);
+
+    #region Dreams scheduled/shown
     private const string dream = "Dream";
-    public const string VisitedFP6times = uniqueprefix + "visitedFP6times";
-
-
-
-    /// <summary>
-    /// Did you know? DreamState.DreamID has .ToString() overridden to write it into save file compactly. For that reason i'd prefer not to use that enum
-    /// </summary>
     public enum Dream
     {
         None,
@@ -36,8 +84,6 @@ public static class SaveManager
         VoidNSH,
         VoidSea
     }
-
-
     public struct DreamData
     {
         public DreamData(bool HasShowConditions = false, bool WasShown = false)
@@ -52,44 +98,6 @@ public static class SaveManager
             return $"met conditions: {HasShowConditions}, was shown: {WasShown}";
         }
     }
-
-    public static bool GetTeleportationDone(this SaveState save) => save.miscWorldSaveData.GetSlugBaseData().TryGet(teleportationDone, out bool done) && done;
-    public static void SetTeleportationDone(this SaveState save, bool value) => save.miscWorldSaveData.GetSlugBaseData().Set(teleportationDone, value);
-
-    public static bool GetMessageShown(this SaveState save) => save.miscWorldSaveData.GetSlugBaseData().TryGet(messageShown, out bool shown) && shown;
-    public static void SetMessageShown(this SaveState save, bool value) => save.miscWorldSaveData.GetSlugBaseData().Set(messageShown, value);
-
-    public static int GetLastMeetCycles(this SaveState save)
-    {
-        var data = save.miscWorldSaveData.GetSlugBaseData();
-        if(!data.TryGet(lastMeetCycles, out int cycles))
-        {
-            cycles = 0;
-            data.Set(lastMeetCycles, 0);
-        }
-        return cycles;
-    }
-    public static void SetLastMeetCycles(this SaveState save, int cycles) => save.miscWorldSaveData.GetSlugBaseData().Set(lastMeetCycles, cycles);
-
-    public static int GetPebblesPearlsEaten(this SaveState save)
-    {
-        var data = save.miscWorldSaveData.GetSlugBaseData();
-        if (!data.TryGet(pebblesPearlsEaten, out int eatenPearlsAmount))
-        {
-            eatenPearlsAmount = 0;
-            data.Set(pebblesPearlsEaten, 0);
-        }
-        return eatenPearlsAmount;
-    }
-    public static void SetPebblesPearlsEaten(this SaveState save, int amount) => save.miscWorldSaveData.GetSlugBaseData().Set(pebblesPearlsEaten, amount);
-
-    public static bool GetVoidCatDead(this SaveState save) => save.miscWorldSaveData.GetSlugBaseData().TryGet(voidCatDead, out bool dead) && dead;
-    public static void SetVoidCatDead(this SaveState progression, bool value) => progression.miscWorldSaveData.GetSlugBaseData().Set(voidCatDead, value);
-
-    public static bool GetEndingEncountered(this SaveState save) => save.miscWorldSaveData.GetSlugBaseData().TryGet(endingDone, out bool done) && done;
-    public static void SetEndingEncountered(this SaveState save, bool value) => save.miscWorldSaveData.GetSlugBaseData().Set(endingDone, value);
-
-    #region Dreams scheduled/shown
     public static DreamData GetDreamData(this SaveState save, Dream dream)
     {
         var data = save.miscWorldSaveData.GetSlugBaseData();

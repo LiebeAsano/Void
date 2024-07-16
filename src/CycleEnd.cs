f53e30bc26f8448fb2c118da4b70d88c911a93fc
Inevitabilis <ddd1020018@gmail.com>
2024-07-16 17:45:05 +0300

    Death On Malnourished

--- src/CycleEnd.cs
+++ src/CycleEnd.cs
@@ -1,25 +1,74 @@
-﻿using MonoMod.RuntimeDetour;
+﻿using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mono.Cecil.Cil;
 
 namespace VoidTemplate;
 
 internal static class CycleEnd
 {
+    private static void log(object e) => _Plugin.logger.LogInfo(e);
     public static void Hook()
     {
         On.ShelterDoor.Close += CycleEndLogic;
-        On.RainWorldGame.Update += RainWorldGame_Update;
+        //On.RainWorldGame.Update += RainWorldGame_Update;
+        //IL.ShelterDoor.Update += ShelterDoor_Update;
+        On.RainWorldGame.Win += RainWorldGame_Win;
+    }
+
+    private static void RainWorldGame_Win(On.RainWorldGame.orig_Win orig, RainWorldGame self, bool malnourished)
+    {
+        if(self.StoryCharacter == StaticStuff.TheVoid)
+        {
+            self.GoToDeathScreen();
+            return;
+        }
+        orig(self, malnourished);
+    }
+
+    private static void ShelterDoor_Update(MonoMod.Cil.ILContext il)
+    {
+        ILCursor c = new(il);
+        var bubblestart = c.DefineLabel();
+        var pastbubble = c.DefineLabel();
+        // this.room.game <if TheVoid campaign, go to bubblestart>.GoToStarveScreen();
+        // < bubblestart
+        // pop
+        // 
+        if (c.TryGotoNext(MoveType.Before,
+            x => x.MatchCallvirt<RainWorldGame>(nameof(RainWorldGame.GoToStarveScreen))))
+        {
+            c.Emit(OpCodes.Ldarg_0);
+            c.EmitDelegate<Func<ShelterDoor, bool>>((self) => (self.room.game.IsStorySession && self.room.game.GetStorySession.characterStats.name == StaticStuff.TheVoid));
+            c.Emit(OpCodes.Brtrue_S, bubblestart);
+        }
+        else
+            _Plugin.logger.LogError($"IL hook starting at CycleEnd:23, shelter door update, starve logic tinker, failed to apply");
+        if (c.TryGotoNext(MoveType.After,
+             x => x.MatchCallvirt<RainWorldGame>(nameof(RainWorldGame.GoToStarveScreen))))
+        {
+            c.Emit(OpCodes.Br, pastbubble);
+            c.MarkLabel(bubblestart);
+            c.EmitDelegate((RainWorldGame game) => game.GoToDeathScreen());
+            c.MarkLabel(pastbubble);
+        }
+        else _Plugin.logger.LogError($"IL hook starting at CycleEnd:41, shelter door update, starve logic tinker, failed to apply");
     }
 
     private static void RainWorldGame_Update(On.RainWorldGame.orig_Update orig, RainWorldGame self)
     {
         orig(self);
         if (timerStarted) timer++;
-        if (timer > timeToWait) self.GoToStarveScreen();
+        if (timer > timeToWait)
+        {
+            self.GoToStarveScreen();
+            timerStarted = false;
+            timer = 0;
+        }
 
     }
 
@@ -46,7 +95,7 @@ internal static class CycleEnd
             && (!ModManager.Expedition || !self.room.game.rainWorld.ExpeditionMode))
             {
                 if (session.saveState.deathPersistentSaveData.karma == 0 || session.saveState.deathPersistentSaveData.karma == 10) game.GoToRedsGameOver();
-                else timerStarted = true;
+                //else timerStarted = true;
             }
         });
     }

b40d8fd4d9bf37d6f99404e2271a8755f99bf917
Inevitabilis <ddd1020018@gmail.com>
2024-07-17 21:47:32 +0300

    Passage Circles disabled

--- src/MenuTinkery/DisablePassage.cs
+++ src/MenuTinkery/DisablePassage.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Mono.Cecil.Cil;
 using Menu;
+using Mono.Cecil;
+using MonoMod.Utils;
 
 namespace VoidTemplate.MenuTinkery;
 
@@ -14,8 +16,8 @@ internal static class DisablePassage
     public static void Hook()
     {
         On.Menu.SleepAndDeathScreen.AddPassageButton += RemoveButtonForVoid;
-        //IL.Menu.SleepAndDeathScreen.GetDataFromGame += SleepAndDeathScreen_GetDataFromGame;
-        On.Menu.SleepAndDeathScreen.GetDataFromGame += SleepAndDeathScreen_GetDataFromGame1;
+        IL.Menu.SleepAndDeathScreen.GetDataFromGame += SleepAndDeathScreen_GetDataFromGame;
+        //On.Menu.SleepAndDeathScreen.GetDataFromGame += SleepAndDeathScreen_GetDataFromGame1;
     }
 
     private static void SleepAndDeathScreen_GetDataFromGame1(On.Menu.SleepAndDeathScreen.orig_GetDataFromGame orig, SleepAndDeathScreen self, KarmaLadderScreen.SleepDeathScreenDataPackage package)
@@ -23,12 +25,25 @@ internal static class DisablePassage
         orig(self, package);
         if(package.characterStats.name == StaticStuff.TheVoid)
         {
-            self.pages[0].subObjects.Remove(self.endgameTokens);
-            self.endgameTokens = null;
+            try
+            {
+                if(self.endgameTokens != null)
+                {
+                self.endgameTokens.RemoveSprites();
+                self.pages[0].subObjects.Remove(self.endgameTokens);
+                self.endgameTokens = null;
+
+                }
+            }
+            catch (Exception e)
+            {
+                logerr(e);
+            }
         }
     }
 
     private static void logerr(object e) => _Plugin.logger.LogError(e);
+    private static void loginf(object e) => _Plugin.logger.LogInfo(e);
     /// <summary>
     /// not working
     /// </summary>
@@ -36,13 +51,15 @@ internal static class DisablePassage
     private static void SleepAndDeathScreen_GetDataFromGame(MonoMod.Cil.ILContext il)
     {
         var c = new ILCursor(il);
+        //i spent an hour figuring out the issue in matching operator in generic. turns out type argument hates ExtEnum
         if (c.TryGotoNext(MoveType.After,
-            q => q.MatchCall<ExtEnum<SlugcatStats.Name>>("op_Inequality")))
+            q => q.MatchCall("ExtEnum`1<SlugcatStats/Name>", "op_Inequality")
+            ))
         {
             c.Emit(OpCodes.Ldarg_1);
             c.EmitDelegate<Func<bool, KarmaLadderScreen.SleepDeathScreenDataPackage, bool>>(static (orig, package) => orig && package.characterStats.name != StaticStuff.TheVoid);
         }
-        else logerr($"IL hook at MenuTinkery.DisablePasssage:24 didn't work");
+        else logerr("IL hook failed to match. MenuTinkery.DisablePassage:54");
     }
 
     private static void RemoveButtonForVoid(On.Menu.SleepAndDeathScreen.orig_AddPassageButton orig, Menu.SleepAndDeathScreen self, bool buttonBlack)

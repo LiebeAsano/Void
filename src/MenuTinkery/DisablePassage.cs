6e7b5ad04e98e63344ec9d3f02c9c69a7be06075
Inevitabilis <ddd1020018@gmail.com>
2024-07-17 18:23:24 +0300

    Passage IL -> On attempt

--- src/MenuTinkery/DisablePassage.cs
+++ src/MenuTinkery/DisablePassage.cs
@@ -14,9 +14,25 @@ internal static class DisablePassage
     public static void Hook()
     {
         On.Menu.SleepAndDeathScreen.AddPassageButton += RemoveButtonForVoid;
-        IL.Menu.SleepAndDeathScreen.GetDataFromGame += SleepAndDeathScreen_GetDataFromGame;
+        //IL.Menu.SleepAndDeathScreen.GetDataFromGame += SleepAndDeathScreen_GetDataFromGame;
+        On.Menu.SleepAndDeathScreen.GetDataFromGame += SleepAndDeathScreen_GetDataFromGame1;
     }
 
+    private static void SleepAndDeathScreen_GetDataFromGame1(On.Menu.SleepAndDeathScreen.orig_GetDataFromGame orig, SleepAndDeathScreen self, KarmaLadderScreen.SleepDeathScreenDataPackage package)
+    {
+        orig(self, package);
+        if(package.characterStats.name == StaticStuff.TheVoid)
+        {
+            self.pages[0].subObjects.Remove(self.endgameTokens);
+            self.endgameTokens = null;
+        }
+    }
+
+    private static void logerr(object e) => _Plugin.logger.LogError(e);
+    /// <summary>
+    /// not working
+    /// </summary>
+    /// <param name="il"></param>
     private static void SleepAndDeathScreen_GetDataFromGame(MonoMod.Cil.ILContext il)
     {
         var c = new ILCursor(il);
@@ -26,6 +42,7 @@ internal static class DisablePassage
             c.Emit(OpCodes.Ldarg_1);
             c.EmitDelegate<Func<bool, KarmaLadderScreen.SleepDeathScreenDataPackage, bool>>(static (orig, package) => orig && package.characterStats.name != StaticStuff.TheVoid);
         }
+        else logerr($"IL hook at MenuTinkery.DisablePasssage:24 didn't work");
     }
 
     private static void RemoveButtonForVoid(On.Menu.SleepAndDeathScreen.orig_AddPassageButton orig, Menu.SleepAndDeathScreen self, bool buttonBlack)

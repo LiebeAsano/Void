f4f3816e6b38da46162e773c78be0353cb40541c
Inevitabilis <ddd1020018@gmail.com>
2024-07-31 17:41:01 +0300

    switching to implicit slotting

--- src/SaveSystem3/SaveSystem3.cs
+++ src/SaveSystem3/SaveSystem3.cs
@@ -5,11 +5,30 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security.Permissions;
+using MonoMod.RuntimeDetour;
 
 namespace VoidTemplate.SaveSystem3;
 
 internal static class SaveSystem3
 {
+    public static void Hook()
+    {
+        On.Menu.InitializationScreen.Update += InitializationScreen_Update;
+    }
+
+    
+    static bool initialized = false;
+    private static void InitializationScreen_Update(On.Menu.InitializationScreen.orig_Update orig, Menu.InitializationScreen self)
+    {
+        orig(self);
+        if(self.manager.rainWorld.OptionsReady && !initialized)
+        {
+            SaveSlot = self.manager.rainWorld.options.saveSlot;
+        }
+    }
+
+
+    static int SaveSlot = -1;
     static Dictionary<string, string> SaveData = new();
     static Dictionary<string, string> DeathPersistentSaveData = new();
     static Dictionary<string, string> MiscWorldSaveData = new();
@@ -20,19 +39,15 @@ internal static class SaveSystem3
         DeathPersistent,
         MiscWorld
     }
-    public static SaveSystem3()
+
+    public static void SaveImminentData()
     {
-        
-        
-    }
-    public static void SaveImminentData(int saveslot)
-    {
-        Parser.WriteDataToDisk(SaveData, saveslot);
+        Parser.WriteDataToDisk([SaveData, DeathPersistentSaveData, MiscWorldSaveData], SaveSlot);
     }
 
 
 
-    
+
 
     static class Parser
     {
@@ -46,17 +61,31 @@ internal static class SaveSystem3
         }
         const string divider = "<DIV>";
 
-        public static void WriteDataToDisk(Dictionary<string, string> data, int slot) => DiskCommunicator.SaveSaveData(slot, PrepareForWrite(data));
-        public static Dictionary<string, string> GetDataFromDisk(int slot) => ParseRawData(DiskCommunicator.LoadSaveData(slot));
+
+        public static void WriteDataToDisk(Dictionary<string, string>[] data, int slot)
+        {
+            List<String> strings = new List<String>();
+            Array.ForEach(data, data =>
+            {
+                var localstrings = PrepareForWrite(data);
+                localstrings.ForEach(x => strings.Add(x));
+                strings.Add("<MAJORDATADIV>");
+            });
+            DiskCommunicator.SaveSaveData(SaveSlot, [.. strings]);
+        }
+        public static Dictionary<string, string> GetDataFromDisk(int slot)
+        {
+            return ParseRawData(DiskCommunicator.LoadSaveData(slot));
+        }
         
-        static string[] PrepareForWrite(Dictionary<string, string> data)
+        static List<string> PrepareForWrite(Dictionary<string, string> data)
         {
             List<string> kvp = [];
             foreach (var key in data.Keys)
             {
                 kvp.Add(key + divider + data[key]);
             }
-            return [.. kvp];
+            return kvp;
         }
         static Dictionary<string, string> ParseRawData(string[] strings)
         {

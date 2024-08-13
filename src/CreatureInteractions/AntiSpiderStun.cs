d9be63315915172b5d499edbf17c725d3f3de920
Inevitabilis <ddd1020018@gmail.com>
2024-08-13 10:22:59 +0300

    comments for readability

--- src/CreatureInteractions/AntiSpiderStun.cs
+++ src/CreatureInteractions/AntiSpiderStun.cs
@@ -1,10 +1,6 @@
 ﻿using MonoMod.Cil;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using Mono.Cecil.Cil;
-using System.Text;
-using System.Threading.Tasks;
 using static VoidTemplate.Useful.Utils;
 
 namespace VoidTemplate.CreatureInteractions;
@@ -15,25 +11,30 @@ public static class AntiSpiderStun
     {
         IL.DartMaggot.Update += DartMaggot_Update;
     }
-
-    private static void DartMaggot_Update(MonoMod.Cil.ILContext il)
+    static void logerr(object e) => _Plugin.logger.LogError(e);
+    //Dart Maggots, the things spider spitter (class bigspider) shoots, have two stuns: the one that happens each tick gradually increases with a maximum of 22 stun application (not += but =)
+    //and then there's the "when destroyed, stun as much as the amount of darts in body, up to 4", which is 40*(2 + amount * 3) so from 200 to 560 stun
+    //quick lookup didn't reveal anything special about as little as 22 stun, so this method only applies to the latter function, "stun when out of poison"
+    private static void DartMaggot_Update(ILContext il)
     {
         ILCursor c = new(il);
-        if(c.TryGotoNext(MoveType.After, 
-            x => x.MatchMul(), 
+        //stun = max( current stun,  40*(2 + amount*3) < if void multiply by karma resist coefficient  > )
+        if (c.TryGotoNext(MoveType.After,
+            x => x.MatchMul(),
             x => x.MatchAdd(),
             x => x.MatchMul()))
         {
             c.Emit(OpCodes.Ldarg_0);
             c.EmitDelegate<Func<int, DartMaggot, int>>((int orig, DartMaggot maggot) =>
             {
-                if(maggot.stuckInChunk.owner is Player p && p.IsVoid())
+                if (maggot.stuckInChunk.owner is Player p && p.IsVoid())
                 {
                     var karma = p.Karma;
                     return (int)((float)orig * (1f - 0.09f * karma));
                 }
                 return orig;
             });
-        }    
+        }
+        else logerr(nameof(CreatureInteractions) + "." + nameof(AntiSpiderStun) + "." + nameof(DartMaggot_Update) + ": the IL hook making Void resistant to spider darts didn't find its place");
     }
 }

93952630ac9fc0d076f4c00809ad119e00a74ef0
Inevitabilis <70079177+Inevitabilis@users.noreply.github.com>
2024-08-21 21:43:01 +0300

    K11 no move & no show other K

--- src/PlayerMechanics/Karma11Foundation/KarmaLadderTweaks.cs
+++ src/PlayerMechanics/Karma11Foundation/KarmaLadderTweaks.cs
@@ -12,8 +12,30 @@ internal static class KarmaLadderTweaks
     public static void Hook()
     {
         On.Menu.KarmaLadder.KarmaSymbol.ctor += KarmaSymbol_ctor;
+        On.Menu.KarmaLadderScreen.FoodCountDownDone += KarmaLadderScreen_FoodCountDownDone;
+    }
+    /// <summary>
+    /// prevents karma 11 from going up or down
+    /// </summary>
+    /// <param name="orig"></param>
+    /// <param name="self"></param>
+    private static void KarmaLadderScreen_FoodCountDownDone(On.Menu.KarmaLadderScreen.orig_FoodCountDownDone orig, Menu.KarmaLadderScreen self)
+    {
+        orig(self);
+        if(self.karma.x == karma11index) self.karmaLadder.GoToKarma(karma11index, false);
     }
 
+    /// <summary>
+    /// Disables the circle around karma 11 for karma ladder and lowers transparency for all the symbols when currently it's karma 11
+    /// </summary>
+    /// <param name="orig"></param>
+    /// <param name="self"></param>
+    /// <param name="menu"></param>
+    /// <param name="owner"></param>
+    /// <param name="pos"></param>
+    /// <param name="container"></param>
+    /// <param name="foregroundContainer"></param>
+    /// <param name="displayKarma"></param>
     private static void KarmaSymbol_ctor(On.Menu.KarmaLadder.KarmaSymbol.orig_ctor orig, Menu.KarmaLadder.KarmaSymbol self, Menu.Menu menu, Menu.MenuObject owner, UnityEngine.Vector2 pos, FContainer container, FContainer foregroundContainer, RWCustom.IntVector2 displayKarma)
     {
         orig(self, menu, owner, pos, container, foregroundContainer, displayKarma); 
@@ -21,5 +43,10 @@ internal static class KarmaLadderTweaks
         {
             self.sprites[self.RingSprite].alpha = 0f;
         }
+        if(self.ladder.displayKarma.x == karma11index && displayKarma.x != karma11index)
+        {
+            Array.ForEach(self.sprites, sprite => sprite.alpha = 0f);
+        }
+
     }
 }

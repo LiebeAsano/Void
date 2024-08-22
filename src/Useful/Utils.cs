7d3d371362bb66d3d0865881cf55c5485ad75634
Inevitabilis <70079177+Inevitabilis@users.noreply.github.com>
2024-08-22 21:07:22 +0300

    loginf and logerr are now part of utils

--- src/Useful/Utils.cs
+++ src/Useful/Utils.cs
@@ -8,6 +8,8 @@ namespace VoidTemplate.Useful;
 internal static class Utils
 {
     public const int TicksPerSecond = 40;
+    public static void loginf(object e) => _Plugin.logger.LogInfo(e);
+    public static void logerr(object e) => _Plugin.logger.LogError(e);
     public static string TranslateStringComplex(this string str) => RWCustom.Custom.rainWorld.inGameTranslator.Translate(str).Replace("<LINE>", "\n");
 
     public static bool IsVoid(this Player p) => p.slugcatStats.name == VoidEnums.SlugcatID.TheVoid;

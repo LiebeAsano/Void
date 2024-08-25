c6ad600e8f63e176e7a158cec4cfe6efd9b7adab
Inevitabilis <ddd1020018@gmail.com>
2024-08-25 20:06:50 +0300

    NRE fix attempt

--- src/PlayerMechanics/GraspSave.cs
+++ src/PlayerMechanics/GraspSave.cs
@@ -14,30 +14,30 @@ internal static class GraspSave
 	public static void Hook()
 	{
 		On.Player.Grabbed += Player_Grabbed;
-        On.Creature.Update += Creature_Update;
+		On.Creature.Update += Creature_Update;
 	}
 
-    private static void Creature_Update(On.Creature.orig_Update orig, Creature self, bool eu)
-    {
+	private static void Creature_Update(On.Creature.orig_Update orig, Creature self, bool eu)
+	{
 		orig(self, eu);
 		if(grabbers.TryGetValue(self.abstractCreature, out var timer))
 		{
-			if(Array.Exists(self.grasps, grasp => grasp.grabbed is Player p && p.IsVoid()))
+			if(Array.Exists(self.grasps, grasp => grasp != null && grasp.grabbed is Player p && p.IsVoid()))
 			{
 				timer.Value++;
 				Player p = Array.Find(self.grasps, grasp => grasp.grabbed is Player p && p.IsVoid()).grabbed as Player;
-                if (timer.Value > TicksUntilDeath(p))
+				if (timer.Value > TicksUntilDeath(p))
 				{
 					self.Die();
 				}
-            }
+			}
 			else grabbers.Remove(self.abstractCreature);
 		}
-    }
+	}
 	static int TicksUntilDeath(Player p)
 	{
 		return TicksPerSecond * (p.KarmaCap == 11 ? 90 : 180);
-    }
+	}
 
 	static ConditionalWeakTable<AbstractCreature, StrongBox<int>> grabbers = new();
 	private static void Player_Grabbed(On.Player.orig_Grabbed orig, Player self, Creature.Grasp grasp)

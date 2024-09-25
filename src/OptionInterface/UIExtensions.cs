bf33c5baae8949a56b674faaa9c67565c68f48fe
Inevitabilis <ddd1020018@gmail.com>
2024-09-25 21:16:36 +0300

    OI cfg autogen

--- src/OptionInterface/UIExtensions.cs
+++ src/OptionInterface/UIExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Menu.Remix.MixedUI;
 using VoidTemplate.Useful;
 using UnityEngine;
+using System;
 
 namespace VoidTemplate.OptionInterface;
 
@@ -27,18 +28,18 @@ internal static class UIExtensions
 	}
 	public static void GenerateColoredCheckbox(this OpTab opTab, Configurable<bool> configurable, Vector2 pos, Color color)
 	{
-        OpCheckBox opCheckBox = new OpCheckBox(configurable, pos)
-        {
-            description = configurable.info.description.TranslateStringComplex()
-        };
-        OpLabel opLabel = new(pos: pos + new Vector2(30, -3),
-            size: new Vector2(240f, 30f),
-            text: (configurable.info.Tags.Length > 0 ? configurable.info.Tags[0] as string : "").TranslateStringComplex(),
-            FLabelAlignment.Left);
+		OpCheckBox opCheckBox = new OpCheckBox(configurable, pos)
+		{
+			description = configurable.info.description.TranslateStringComplex()
+		};
+		OpLabel opLabel = new(pos: pos + new Vector2(30, -3),
+			size: new Vector2(240f, 30f),
+			text: (configurable.info.Tags.Length > 0 ? configurable.info.Tags[0] as string : "").TranslateStringComplex(),
+			FLabelAlignment.Left);
 		opCheckBox.colorEdge = color;
 		opLabel.color = color;
-        opTab.AddItems([opCheckBox, opLabel]);
-    }
+		opTab.AddItems([opCheckBox, opLabel]);
+	}
 	public static void GenerateBigText(this OpTab opTab, string text, Vector2 pos)
 	{
 		OpLabel opLabel = new(pos: pos,
@@ -46,6 +47,32 @@ internal static class UIExtensions
 			size: new Vector2(260, 30),
 			bigText: true);
 		opTab.AddItems(opLabel);
+	}
 
+	const int marginBetweenVerticalElements = 30;
+	/// <summary>
+	/// Generates block of big label and config parameters. Starting point - top left part of rectangle, label gets additional offset
+	/// </summary>
+	/// <param name="opTab"></param>
+	/// <param name="label"></param>
+	/// <param name="startingPosition"></param>
+	/// <param name="labelHorizontalOffset"></param>
+	/// <param name="options"></param>
+	/// <exception cref="NotImplementedException"></exception>
+	public static void GenerateBlock(this OpTab opTab, string label, Vector2 startingPosition, int labelHorizontalOffset = 10, params (ConfigurableBase, Color)[] options)
+	{
+		opTab.GenerateBigText(label, startingPosition + new Vector2(labelHorizontalOffset, 0));
+		int offset = marginBetweenVerticalElements;
+		foreach (var configColorPair in options)
+		{
+			if (configColorPair.Item1 is Configurable<bool> boolcfg)
+			{
+				opTab.GenerateColoredCheckbox(boolcfg, startingPosition - new Vector2(0, offset), configColorPair.Item2);
+			}
+			else throw new NotImplementedException($"Generating OI block failed due to unsupported configurable type passed in. The passed config is {configColorPair.Item1.key}" +
+				$" and its type is {configColorPair.Item1.GetType()}");
+
+			offset += marginBetweenVerticalElements;
+		}
 	}
 }
--- src/OptionInterface/VoidOptionInterface.cs
+++ src/OptionInterface/VoidOptionInterface.cs
@@ -1,29 +1,31 @@
 ﻿using Menu.Remix.MixedUI;
 using VoidTemplate.Useful;
 using UnityEngine;
-using RWCustom;
-using Menu.Remix;
 using static VoidTemplate.Useful.Utils;
 using static VoidTemplate.OptionInterface.OptionAccessors;
-using System;
 
 namespace VoidTemplate.OptionInterface;
 
 internal class VoidOptionInterface : global::OptionInterface
 {
 	const int marginBetweenVerticalElements = 30;
+	static Color MediumGrey = new Color(0.66f, 0.64f, 0.70f);
+	static Color CheatingColor = new Color(0.85f, 0.35f, 0.4f);
+
 	public override void Initialize()
 	{
 		base.Initialize();
 		Tabs = [new OpTab(this, "blahblahblah")];
-		Tabs[0].GenerateBigText("~ Arena ~".TranslateStringComplex(), new Vector2(60, 550));
-		Tabs[0].GenerateCheckbox(cfgSaintArenaAscension, new Vector2(50,550-marginBetweenVerticalElements));
-		Tabs[0].GenerateCheckbox(cfgSaintArenaSpears, new Vector2(50, 550-marginBetweenVerticalElements*2));
-
-		Tabs[0].GenerateCheckbox(cfgSimpleFood, new Vector2(50,550 - marginBetweenVerticalElements*4));
-		Tabs[0].GenerateColoredCheckbox(cfgPermaDeath, new Vector2(50, 550 - marginBetweenVerticalElements * 5), new Color(0.85f, 0.35f, 0.4f));
-		Tabs[0].GenerateColoredCheckbox(cfgForceUnlockCampaign, new Vector2(50, 550 - marginBetweenVerticalElements * 6), new Color(0.85f, 0.35f, 0.4f));
-    }
+		Tabs[0].GenerateBlock("~ Arena ~".TranslateStringComplex(), new Vector2(50, 550), options: [
+			(cfgSaintArenaAscension, MediumGrey),
+			(cfgSaintArenaSpears, MediumGrey),
+			]);
+		Tabs[0].GenerateBlock("~ Assist ~".TranslateStringComplex(), new Vector2(50, 430), options: [
+			(cfgSimpleFood, MediumGrey),
+			(cfgPermaDeath, CheatingColor),
+			(cfgForceUnlockCampaign, CheatingColor)
+			]);
+	}
 
 
 }

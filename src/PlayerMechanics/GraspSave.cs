33b032ed5aa1fa4ebe6fc4fb13cd94b49707a0f1
Inevitabilis <ddd1020018@gmail.com>
2024-10-17 12:56:50 +0300

    potential graspsave fix

--- src/PlayerMechanics/GraspSave.cs
+++ src/PlayerMechanics/GraspSave.cs
@@ -7,38 +7,53 @@ namespace VoidTemplate.PlayerMechanics;
 internal static class GraspSave
 {
 
-	public static void Hook()
-	{
-		On.Player.Grabbed += Player_Grabbed;
-		On.Creature.Update += Creature_Update;
-	}
+    public static void Hook()
+    {
+        On.Player.Grabbed += Player_Grabbed;
+        On.Creature.Update += Creature_Update;
+    }
 
-	private static void Creature_Update(On.Creature.orig_Update orig, Creature self, bool eu)
-	{
-		orig(self, eu);
-		if (grabbers.TryGetValue(self.abstractCreature, out var timer))
-		{
-			if (Array.Exists(self.grasps, grasp => grasp != null && grasp.grabbed is Player p && p.IsVoid()))
-			{
-				timer.Value++;
-				Player p = Array.Find(self.grasps, grasp => grasp != null && grasp.grabbed is Player p && p.IsVoid()).grabbed as Player;
-				if (timer.Value > TicksUntilDeath(p))
-				{
-					self.Stun(TicksPerSecond * 5);
-				}
-			}
-			else grabbers.Remove(self.abstractCreature);
-		}
-	}
-	static int TicksUntilDeath(Player p)
-	{
-		return TicksPerSecond * (p.KarmaCap == 10 ? 30 : 45);
-	}
+    private static void Creature_Update(On.Creature.orig_Update orig, Creature self, bool eu)
+    {
+        orig(self, eu);
+        if (grabbers.TryGetValue(self.abstractCreature, out var grabbedCreatures))
+        {
+            Array.ForEach(self.grasps, grasp =>
+            {
+                if (grasp != null
+            && grasp.grabbed is Player p
+            && p.IsVoid()
+            && grabbedCreatures.TryGetValue(p.abstractCreature, out var timer))
+                {
+                    timer.Value++;
+                    if (timer.Value > TicksUntilDeath(p))
+                    {
+                        self.Stun(TicksPerSecond * 5);
+                    }
+                }
+            });
 
-	static ConditionalWeakTable<AbstractCreature, StrongBox<int>> grabbers = new();
-	private static void Player_Grabbed(On.Player.orig_Grabbed orig, Player self, Creature.Grasp grasp)
-	{
-		orig(self, grasp);
-		if (self.IsVoid() && !grabbers.TryGetValue(grasp.grabber.abstractCreature, out var _)) grabbers.Add(grasp.grabber.abstractCreature, new(0));
-	}
+        }
+    }
+
+    static int TicksUntilDeath(Player p)
+    {
+        return TicksPerSecond * (p.KarmaCap == 10 ? 40 : 60);
+    }
+
+    static ConditionalWeakTable<AbstractCreature, ConditionalWeakTable<AbstractCreature, StrongBox<int>>> grabbers = new();
+    private static void Player_Grabbed(On.Player.orig_Grabbed orig, Player self, Creature.Grasp grasp)
+    {
+        orig(self, grasp);
+        if (self.IsVoid())
+        {
+            ConditionalWeakTable<AbstractCreature, StrongBox<int>> grabbedCreatures;
+            if (!grabbers.TryGetValue(grasp.grabber.abstractCreature, out grabbedCreatures))
+            {
+                grabbedCreatures = new();
+            }
+            grabbedCreatures.Add(self.abstractCreature, new(0));
+
+        }
+    }
 }

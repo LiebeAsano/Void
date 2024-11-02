689b9cb680115de489f81c53c661346a3e8762c9
Liebe Asano <liebeasano@gmail.com>
2024-11-02 13:31:53 +0300

    Grab update

--- src/PlayerMechanics/CanMaul.cs
+++ src/PlayerMechanics/CanMaul.cs
@@ -5,8 +5,13 @@ using RWCustom;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
+using VoidTemplate.Useful;
+using static VoidTemplate.Useful.Utils;
 
 namespace VoidTemplate.PlayerMechanics;
 
@@ -15,14 +20,47 @@ internal static class СanMaul
     public static void Hook()
     {
         On.Player.CanMaulCreature += CanMaulCreatureHook;
+        IL.Player.GrabUpdate += Player_GrabUpdate;
     }
 
     private static bool CanMaulCreatureHook(On.Player.orig_CanMaulCreature orig, Player self, Creature crit)
     {
         if (crit is Player && !crit.dead && self != null && self.slugcatStats.name == VoidEnums.SlugcatID.Void)
         {
-            return true; 
+            return true;
         }
-        return orig(self,crit);
+        return orig(self, crit);
+    }
+
+    private static void Player_GrabUpdate(ILContext il)
+    {
+        ILCursor c = new ILCursor(il);
+
+        if (c.TryGotoNext(
+        MoveType.After,
+        x => x.MatchCallOrCallvirt(typeof(Creature).GetMethod("Violence"))))
+        {
+
+            c.Emit(OpCodes.Ldarg_0);
+
+            c.EmitDelegate<Action<Creature>>(creature =>
+            {
+                Array.ForEach(creature.grasps, grasp =>
+                {
+                    if (grasp != null
+                        && grasp.grabbed is Player playerInGrasp
+                        && playerInGrasp.IsVoid()
+                        && creature is Player player 
+                        && player.slugcatStats.name != VoidEnums.SlugcatID.Void)
+                    {
+                        creature.Stun(TicksPerSecond * 5);
+                    }
+                });
+            });
+        }
+        else
+        {
+            LogExErr("Failed to find the call to Player_GrabUpdate injection point not found.");
+        }
     }
 }

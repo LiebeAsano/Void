fc7ec293accef916083b2476f2c8756fac812ce8
Inevitabilis <ddd1020018@gmail.com>
2024-11-24 21:30:36 +0300

    sound fadeout in statistics screen

--- src/MenuTinkery/MenuStatisticsSound.cs
+++ src/MenuTinkery/MenuStatisticsSound.cs
@@ -1,4 +1,5 @@
 ﻿using MonoMod.RuntimeDetour;
+using Music;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,32 +12,47 @@ namespace VoidTemplate.MenuTinkery;
 
 internal static class MenuStatisticsSound
 {
-    static Dictionary<string, SoundID> StatisticSoundMap;
-    public static void Hook()
-    {
-        On.Menu.StoryGameStatisticsScreen.GetDataFromGame += GetDataFromGame_Update;
+	static Dictionary<string, SoundID> StatisticSoundMap;
+	public static void Hook()
+	{
+		On.Menu.StoryGameStatisticsScreen.GetDataFromGame += GetDataFromGame_Update;
+		On.Menu.KarmaLadderScreen.Singal += KarmaLadderScreen_Singal;
 
-        StatisticSoundMap = new()
-        {
-            { "Static_Death_Void",  VoidEnums.SoundID.StaticDeathSound },
-            { "Static_Death_Void11",  VoidEnums.SoundID.StaticDeathSound11 },
-            { "Static_End_Scene_Void", VoidEnums.SoundID.StaticEndSound },
-            { "Static_End_Scene_Void11", VoidEnums.SoundID.StaticEndSound11 },
+		StatisticSoundMap = new()
+		{
+			{ "Static_Death_Void",  VoidEnums.SoundID.StaticDeathSound },
+			{ "Static_Death_Void11",  VoidEnums.SoundID.StaticDeathSound11 },
+			{ "Static_End_Scene_Void", VoidEnums.SoundID.StaticEndSound },
+			{ "Static_End_Scene_Void11", VoidEnums.SoundID.StaticEndSound11 },
+		};
+	}
 
-        }; 
-    }
+	private static void KarmaLadderScreen_Singal(On.Menu.KarmaLadderScreen.orig_Singal orig, Menu.KarmaLadderScreen self, Menu.MenuObject sender, string message)
+	{
+		orig(self, sender, message);
+		if ((message == "CONTINUE"
+			|| message == "EXIT")
+			&& self.manager.musicPlayer is MusicPlayer p
+			&& StatisticSoundMap.Values.Any(value => value.value == p.song.name))
+		{
+			p.song.FadeOut(20f);
+		}
+	}
 
-    private static void GetDataFromGame_Update(On.Menu.StoryGameStatisticsScreen.orig_GetDataFromGame orig, Menu.StoryGameStatisticsScreen self, Menu.KarmaLadderScreen.SleepDeathScreenDataPackage package)
-    {
-        if (package.saveState.saveStateNumber == VoidEnums.SlugcatID.Void)
-        {
-            string currentScene = self.scene.sceneID.value;
+	private static void GetDataFromGame_Update(On.Menu.StoryGameStatisticsScreen.orig_GetDataFromGame orig, Menu.StoryGameStatisticsScreen self, Menu.KarmaLadderScreen.SleepDeathScreenDataPackage package)
+	{
+		if (package.saveState.saveStateNumber == VoidEnums.SlugcatID.Void)
+		{
+			string currentScene = self.scene.sceneID.value;
 
-            if (StatisticSoundMap.Keys.Contains(currentScene))
-            {
-                self.PlaySound(StatisticSoundMap[currentScene]);
-            }
-        }
-        orig(self, package);
-    }
+			if (StatisticSoundMap.Keys.Contains(currentScene)
+				&& self.manager.musicPlayer is MusicPlayer player)
+			{
+				player.song = new(player, StatisticSoundMap[currentScene].value, MusicPlayer.MusicContext.Menu);
+				player.song.playWhenReady = true;
+
+			}
+		}
+		orig(self, package);
+	}
 }

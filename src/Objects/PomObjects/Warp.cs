2b961f34b3250e8a64d89b560443630900d46483
Inevitabilis <ddd1020018@gmail.com>
2024-12-18 21:12:33 +0300

    fixed warp killing room camera when transitioning from non-first loaded room

--- src/Objects/PomObjects/Warp.cs
+++ src/Objects/PomObjects/Warp.cs
@@ -5,6 +5,7 @@ using MoreSlugcats;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using UnityEngine;
 using VoidTemplate.Useful;
@@ -25,11 +26,120 @@ internal class Warp : UpdatableAndDeletable
 			];
 		RegisterFullyManagedObjectType(exposedFields, typeof(Warp), category: "The Void");
 		WarpDestination.Register();
-		
+        On.OverWorld.Update += OverWorld_Update; //since we are doing index based 
 	}
 
-	#region exposed data IDs
-	const string triggerZone = "trigger zone";
+	//Swapping the world directly from UAD yields a side effect:
+	//RWG
+	//for (int m = this.world.activeRooms.Count - 1; m >= 0; m--)
+	//{
+	//		this.world.activeRooms[m].Update();
+	//		this.world.activeRooms[m].PausedUpdate();
+	//}
+	//
+	//where world => this.overworld.world
+	//so when one swaps worlds from within UAD, update index stays
+	//but the number of active rooms drops
+	//which leads to index out of range
+	static ConditionalWeakTable<OverWorld, StrongBox<(WorldLoader, Warp, string)>> customLoader = new();
+private static void OverWorld_Update(On.OverWorld.orig_Update orig, OverWorld self)
+    {
+		orig(self);
+		if(customLoader.TryGetValue(self, out var loader))
+		{
+
+            AbstractRoom absRoom = WorldLoaded(self, loader.Value);
+			loader.Value.Item2.OnRoomChange(absRoom);
+			customLoader.Remove(self);
+			GC.Collect();
+		}
+
+
+        //if you dare to use this method, be aware that players realize at (10;10) coordinate
+        //it used to be (0;0), which really threw off camera logic
+        //after using this, change slugcat positions and apply camera change
+        AbstractRoom WorldLoaded(OverWorld overWorld, (WorldLoader, Warp, string) args)
+        {
+            World world = overWorld.activeWorld;
+            World world2 = args.Item1.ReturnWorld();
+            AbstractRoom abstractRoom2 = null;
+            abstractRoom2 = world2.GetAbstractRoom(args.Item3);
+            overWorld.activeWorld = world2;
+            if (overWorld.game.roomRealizer != null)
+            {
+                overWorld.game.roomRealizer = new RoomRealizer(overWorld.game.roomRealizer.followCreature, world2);
+            }
+
+            abstractRoom2.RealizeRoom(world2, overWorld.game);
+
+            foreach (AbstractCreature absPly in overWorld.game.Players)
+            {
+                world.GetAbstractRoom(absPly.pos).RemoveEntity(absPly);
+                absPly.world = world2;
+                WorldCoordinate worldCoordinate = new(abstractRoom2.index, 10, 10, -1);
+                absPly.pos = worldCoordinate;
+                abstractRoom2.AddEntity(absPly);
+                if (absPly.realizedCreature is Player p
+                    && p.room is not null)
+                {
+                    p.room.RemoveObject(p);
+                    p.PlaceInRoom(abstractRoom2.realizedRoom);
+                    if (p.objectInStomach is not null) p.objectInStomach.world = world2;
+                }
+            }
+
+            foreach (var camera in overWorld.game.cameras)
+            {
+                camera.virtualMicrophone.AllQuiet();
+
+                camera.MoveCamera(abstractRoom2.realizedRoom, 0);
+                camera.ApplyPositionChange();
+                camera.GetCameraBestIndex();
+
+                var hud = camera.hud;
+                hud.ResetMap(new(world2, overWorld.game.rainWorld));
+                camera.dayNightNeedsRefresh = true;
+                if (hud.textPrompt.subregionTracker is not null) hud.textPrompt.subregionTracker.lastShownRegion = 0;
+
+            }
+
+            overWorld.worldLoader = null;
+
+            if (world.regionState is not null) world.regionState.world = null;
+
+            world2.rainCycle.baseCycleLength = world.rainCycle.baseCycleLength;
+            world2.rainCycle.cycleLength = world.rainCycle.cycleLength;
+            world2.rainCycle.timer = world.rainCycle.timer;
+            world2.rainCycle.duskPalette = world.rainCycle.duskPalette;
+            world2.rainCycle.nightPalette = world.rainCycle.nightPalette;
+            world2.rainCycle.dayNightCounter = world.rainCycle.dayNightCounter;
+
+            if (ModManager.MSC)
+            {
+                if (world.rainCycle.timer == 0)
+                {
+                    world2.rainCycle.preTimer = world.rainCycle.preTimer;
+                    world2.rainCycle.maxPreTimer = world.rainCycle.maxPreTimer;
+                }
+                else
+                {
+                    world2.rainCycle.preTimer = 0;
+                    world2.rainCycle.maxPreTimer = 0;
+                }
+            }
+
+            if (!world2.activeRooms.Contains(abstractRoom2.realizedRoom)) world2.activeRooms.Add(abstractRoom2.realizedRoom);
+
+            if (ModManager.MMF)
+            {
+                GC.Collect();
+            }
+            return abstractRoom2;
+        }
+    }
+
+    #region exposed data IDs
+    const string triggerZone = "trigger zone";
 	const string targetRoomName = "TargetRoomName";
 	const string timeToFadeIn = "timetofadein";
 	const string timeToFadeOut = "timetofadeout";
@@ -67,6 +177,7 @@ internal class Warp : UpdatableAndDeletable
 		awaiting,
 		fadein,
 		awaitingWorld,
+		awaitingTransition,
 		awaitingPOMLoad,
 		over
 	}
@@ -81,14 +192,13 @@ internal class Warp : UpdatableAndDeletable
 				&& PositionWithinPoly(TriggerZone, realizedPlayerInRoom.mainBodyChunk.pos) 
 				&& realizedPlayerInRoom.KarmaCap >= 4))
 				{
-					state = State.fadein;
 					room.game.cameras[0].EnterCutsceneMode(room.PlayersInRoom[0].abstractCreature, RoomCamera.CameraCutsceneType.EndingOE);
 					fadeOut = new FadeOut(room, Color.black, duration: TimeToFadeIn, fadeIn: false);
 					room.AddObject(fadeOut);
-
 					threadedLoading = new(this, room, Acronym);
 					thread = new Thread(new ThreadStart(threadedLoading.Load));
 					thread.Start();
+					state = State.fadein;
 				}
 				break;
 
@@ -104,18 +214,13 @@ internal class Warp : UpdatableAndDeletable
 					&& worldLoader.ReturnWorld() is not null)
 				{
 					OverWorld overWorld = room.game.overWorld;
-					overWorld.worldLoader = worldLoader;
+					customLoader.Add(overWorld, new((worldLoader, this, TargetRoom)));
 					worldLoader = null;
-					AbstractRoom destinationRoom = WorldLoaded(overWorld);
-
-					room.updateList.Remove(this);
-					destinationRoom.realizedRoom.AddObject(this);
-					room = destinationRoom.realizedRoom;
-
-					fadeOut = new FadeOut(room, Color.black, TimeToFadeOut, true);
-					room.AddObject(fadeOut);
-					state = State.awaitingPOMLoad;
+					state = State.awaitingTransition;
 				}
+				break;
+
+			case State.awaitingTransition:
 				break;
 
 			case State.awaitingPOMLoad:
@@ -153,88 +258,19 @@ internal class Warp : UpdatableAndDeletable
 				}
 				break;
 		}
-		//if you dare to use this method, be aware that players realize at (10;10) coordinate
-		//it used to be (0;0), which really threw off camera logic
-		//after using this, change slugcat positions and apply camera change
-		AbstractRoom WorldLoaded(OverWorld overWorld)
-		{
-			World world = overWorld.activeWorld;
-			World world2 = overWorld.worldLoader.ReturnWorld();
-			AbstractRoom abstractRoom2 = null;
-			abstractRoom2 = world2.GetAbstractRoom(TargetRoom);
-			overWorld.activeWorld = world2;
-			if (overWorld.game.roomRealizer != null)
-			{
-				overWorld.game.roomRealizer = new RoomRealizer(overWorld.game.roomRealizer.followCreature, world2);
-			}
-
-			abstractRoom2.RealizeRoom(world2, overWorld.game);
-
-			foreach (AbstractCreature absPly in overWorld.game.Players)
-			{
-				world.GetAbstractRoom(absPly.pos).RemoveEntity(absPly);
-				absPly.world = world2;
-				WorldCoordinate worldCoordinate = new(abstractRoom2.index, 10, 10, -1);
-				absPly.pos = worldCoordinate;
-				abstractRoom2.AddEntity(absPly);
-				if (absPly.realizedCreature is Player p
-					&& p.room is not null)
-				{
-					p.room.RemoveObject(p);
-					p.PlaceInRoom(abstractRoom2.realizedRoom);
-					if (p.objectInStomach is not null) p.objectInStomach.world = world2;
-				}
-			}
-
-			foreach (var camera in overWorld.game.cameras)
-			{
-				camera.virtualMicrophone.AllQuiet();
-
-				camera.MoveCamera(abstractRoom2.realizedRoom, 0);
-				camera.ApplyPositionChange();
-				camera.GetCameraBestIndex();
-
-				var hud = camera.hud;
-				hud.ResetMap(new(world2, overWorld.game.rainWorld));
-				camera.dayNightNeedsRefresh = true;
-				if (hud.textPrompt.subregionTracker is not null) hud.textPrompt.subregionTracker.lastShownRegion = 0;
-
-			}
-
-			overWorld.worldLoader = null;
-
-			if (world.regionState is not null) world.regionState.world = null;
-
-			world2.rainCycle.baseCycleLength = world.rainCycle.baseCycleLength;
-			world2.rainCycle.cycleLength = world.rainCycle.cycleLength;
-			world2.rainCycle.timer = world.rainCycle.timer;
-			world2.rainCycle.duskPalette = world.rainCycle.duskPalette;
-			world2.rainCycle.nightPalette = world.rainCycle.nightPalette;
-			world2.rainCycle.dayNightCounter = world.rainCycle.dayNightCounter;
-
-			if (ModManager.MSC)
-			{
-				if (world.rainCycle.timer == 0)
-				{
-					world2.rainCycle.preTimer = world.rainCycle.preTimer;
-					world2.rainCycle.maxPreTimer = world.rainCycle.maxPreTimer;
-				}
-				else
-				{
-					world2.rainCycle.preTimer = 0;
-					world2.rainCycle.maxPreTimer = 0;
-				}
-			}
-
-			if(!world2.activeRooms.Contains(abstractRoom2.realizedRoom)) world2.activeRooms.Add(abstractRoom2.realizedRoom);
-
-			if (ModManager.MMF)
-			{
-				GC.Collect();
-			}
-			return abstractRoom2;
-		}
+		
+		
 	}
+	public void OnRoomChange(AbstractRoom destinationRoom)
+	{
+        room.updateList.Remove(this);
+        destinationRoom.realizedRoom.AddObject(this);
+        room = destinationRoom.realizedRoom;
+
+        fadeOut = new FadeOut(room, Color.black, TimeToFadeOut, true);
+        room.AddObject(fadeOut);
+        state = State.awaitingPOMLoad;
+    }
 
 	class ThreadedLoading(Warp warp, Room room, string targetRegionAcronym)
 	{

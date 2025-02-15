6bd711b7e454c4438f37d0f45d8f7a48f44079b7
Inevitabilis <ddd1020018@gmail.com>
2025-02-15 09:28:39 +0300

    slight cleanup

--- src/PlayerMechanics/ViyMechanics/VoidViySwitch.cs
+++ src/PlayerMechanics/ViyMechanics/VoidViySwitch.cs
@@ -9,48 +9,41 @@ namespace VoidTemplate.PlayerMechanics.ViyMechanics;
 
 internal static class VoidViySwitch
 {
-    public static void Hook()
-    {
-        On.Menu.SlugcatSelectMenu.SetSlugcatColorOrder += SlugcatSelectMenu_SetSlugcatColorOrder;
-    }
+	public static void Hook()
+	{
+		On.Menu.SlugcatSelectMenu.SetSlugcatColorOrder += SlugcatSelectMenu_SetSlugcatColorOrder;
+	}
 
-    private static void SlugcatSelectMenu_SetSlugcatColorOrder(On.Menu.SlugcatSelectMenu.orig_SetSlugcatColorOrder orig, Menu.SlugcatSelectMenu self)
-    {
-        orig(self);
+	private static void SlugcatSelectMenu_SetSlugcatColorOrder(On.Menu.SlugcatSelectMenu.orig_SetSlugcatColorOrder orig, Menu.SlugcatSelectMenu self)
+	{
+		orig(self);
 
-        bool voidDead = SaveManager.ExternalSaveData.VoidDead;
+		bool voidDead = SaveManager.ExternalSaveData.VoidDead;
 
-        self.slugcatColorOrder.RemoveAll(slugcat => slugcat == VoidEnums.SlugcatID.Void || slugcat == VoidEnums.SlugcatID.Viy);
+		self.slugcatColorOrder.RemoveAll(slugcat => slugcat == VoidEnums.SlugcatID.Void || slugcat == VoidEnums.SlugcatID.Viy);
 
 
-        int indexOfHunter = self.slugcatColorOrder.IndexOf(SlugcatStats.Name.Red);
+		int indexOfHunter = self.slugcatColorOrder.IndexOf(SlugcatStats.Name.Red);
 
-        if (indexOfHunter >= 0)
-        {
-            if (voidDead)
-            {
-                self.slugcatColorOrder.Insert(indexOfHunter + 1, VoidEnums.SlugcatID.Viy);
-            }
-            else
-            {
-                self.slugcatColorOrder.Insert(indexOfHunter + 1, VoidEnums.SlugcatID.Void);
-            }
-        }
+		if (indexOfHunter >= 0)
+		{
+			if (voidDead)
+			{
+				self.slugcatColorOrder.Insert(indexOfHunter + 1, VoidEnums.SlugcatID.Viy);
+			}
+			else
+			{
+				self.slugcatColorOrder.Insert(indexOfHunter + 1, VoidEnums.SlugcatID.Void);
+			}
+		}
 
-        var selectedSlugcat = self.manager.rainWorld.progression.miscProgressionData.currentlySelectedSinglePlayerSlugcat;
+		var selectedSlugcat = self.manager.rainWorld.progression.miscProgressionData.currentlySelectedSinglePlayerSlugcat;
 
-        if (!self.slugcatColorOrder.Contains(selectedSlugcat))
-        {
-            if (self.slugcatColorOrder.Count > 0)
-            {
-                self.slugcatPageIndex = 0;
-            }
-        }
-
-        if (self.slugcatPageIndex < 0 || self.slugcatPageIndex >= self.slugcatColorOrder.Count)
-        {
-            self.slugcatPageIndex = 0;
-        }
-
-    }
+		if (!self.slugcatColorOrder.Contains(selectedSlugcat) && self.slugcatColorOrder.Count > 0
+			|| self.slugcatPageIndex < 0
+			|| self.slugcatPageIndex >= self.slugcatColorOrder.Count)
+		{
+			self.slugcatPageIndex = 0;
+		}
+	}
 }

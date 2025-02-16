97abfe677646e11fd197d36a7362431874a169c4
Liebe Asano <liebeasano@gmail.com>
2025-02-16 14:52:48 +0300

    Correct Viy Switch

--- src/PlayerMechanics/ViyMechanics/VoidViySwitch.cs
+++ src/PlayerMechanics/ViyMechanics/VoidViySwitch.cs
@@ -19,15 +19,16 @@ internal static class VoidViySwitch
 		orig(self);
 
 		bool voidDead = SaveManager.ExternalSaveData.VoidDead;
+		bool voidKarma11 = SaveManager.ExternalSaveData.VoidKarma11;
 
-		self.slugcatColorOrder.RemoveAll(slugcat => slugcat == VoidEnums.SlugcatID.Void || slugcat == VoidEnums.SlugcatID.Viy);
+        self.slugcatColorOrder.RemoveAll(slugcat => slugcat == VoidEnums.SlugcatID.Void || slugcat == VoidEnums.SlugcatID.Viy);
 
 
 		int indexOfHunter = self.slugcatColorOrder.IndexOf(SlugcatStats.Name.Red);
 
 		if (indexOfHunter >= 0)
 		{
-			if (voidDead)
+			if (voidDead && voidKarma11)
 			{
 				self.slugcatColorOrder.Insert(indexOfHunter + 1, VoidEnums.SlugcatID.Viy);
 			}

917a26a6b5f3bbc58c16a7be3c9d9066314f93dd
Inevitabilis <ddd1020018@gmail.com>
2025-03-20 10:20:09 +0300

    Disabled soundloop in sleep and death screen

---
+++ src/MenuTinkery/NoSleepSoundLoop.cs
@@ -0,0 +1,25 @@
+﻿namespace VoidTemplate.MenuTinkery;
+using static VoidEnums.SlugcatID;
+
+internal static class NoSleepSoundLoop
+{
+	public static void Hook()
+	{
+		On.Menu.SleepAndDeathScreen.GetDataFromGame += SleepAndDeathScreen_GetDataFromGame;
+	}
+
+	private static void SleepAndDeathScreen_GetDataFromGame(On.Menu.SleepAndDeathScreen.orig_GetDataFromGame orig, Menu.SleepAndDeathScreen self, Menu.KarmaLadderScreen.SleepDeathScreenDataPackage package)
+	{
+		orig(self, package);
+		var slugcatID = package.characterStats.name;
+		if(slugcatID == Void || slugcatID == Viy)
+		{
+			if(self.soundLoop != null)
+			{
+				self.soundLoop.Destroy();
+			}
+			//you can assign your own self.mySoundLoopID here
+			self.mySoundLoopID = SoundID.None;
+		}
+	}
+}

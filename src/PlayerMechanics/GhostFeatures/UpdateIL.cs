40f79be75a394fa8a6d555d59b10205b5e5db564
Liebe Asano <liebeasano@gmail.com>
2025-04-26 01:08:51 +0300

    IL Ghost Update Fix

--- src/PlayerMechanics/GhostFeatures/UpdateIL.cs
+++ src/PlayerMechanics/GhostFeatures/UpdateIL.cs
@@ -9,36 +9,50 @@ namespace VoidTemplate.PlayerMechanics.GhostFeatures;
 
 internal static class UpdateIL
 {
-	public static void Hook()
-	{
-		IL.Ghost.Update += Ghost_UpdateIL;
-	}
+    public static void Hook()
+    {
+        IL.Ghost.Update += Ghost_UpdateIL;
+    }
 
-	private static void Ghost_UpdateIL(ILContext il)
-	{
-		try
-		{
-			ILCursor c = new ILCursor(il);
-			if (c.TryGotoNext(MoveType.After, i => i.MatchLdfld<StoryGameSession>("saveStateNumber"),
-				i => i.MatchLdsfld<MoreSlugcatsEnums.SlugcatStatsName>("Saint"),
-				i => i.MatchCall(out var call) && call.Name.Contains("op_Equality")))
-			{
-				var label = (ILLabel)c.Next.Operand;
-				Debug.Log(label.Target);
-				c.Emit(OpCodes.Ldarg_0);
-				c.EmitDelegate<Func<bool, Ghost, bool>>((re, self) =>
-					re || ((self.room.game.session is StoryGameSession session) &&
-						   session.saveStateNumber == VoidEnums.SlugcatID.Void));
-			}
-			else
-			{
-                LogExErr("&IL.Ghost.Update += Ghost_UpdateIL error IL Hook");
+    private static void Ghost_UpdateIL(ILContext il)
+    {
+        try
+        {
+            ILCursor c = new ILCursor(il);
+
+            if (c.TryGotoNext(MoveType.After,
+                x => x.MatchLdsfld<MoreSlugcats.MoreSlugcatsEnums.SlugcatStatsName>("Saint"),
+                x => x.MatchCall(typeof(ExtEnum<SlugcatStats.Name>).GetMethod("op_Equality"))))
+            {
+                ILLabel skipLabel = il.DefineLabel();
+
+                c.Index -= 2;
+
+                c.Emit(OpCodes.Ldarg_0);
+                c.EmitDelegate<Func<Ghost, bool>>(self =>
+                {
+                    if (self.room.game.session is StoryGameSession session)
+                    {
+                        return session.saveStateNumber == VoidEnums.SlugcatID.Void;
+                    }
+                    return false;
+                });
+
+                c.Emit(OpCodes.Brtrue, skipLabel);
+
+                c.Index += 2;
+
+                c.MarkLabel(skipLabel);
             }
-
+            else
+            {
+                LogExErr("IL.Ghost.Update += Ghost_UpdateIL error: Could not find Saint check");
+            }
         }
-		catch (Exception e)
-		{
-			Debug.LogException(e);
-		}
-	}
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+            LogExErr($"IL.Ghost.Update += Ghost_UpdateIL error: {e}");
+        }
+    }
 }

19985126db8c7b214038920dc61ff0763e603ff5
Inevitabilis <ddd1020018@gmail.com>
2025-06-03 09:25:43 +0300

    shortcut warp

--- src/Objects/PomObjects/Warp.cs
+++ src/Objects/PomObjects/Warp.cs
@@ -1,10 +1,8 @@
 ﻿using MoreSlugcats;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text.RegularExpressions;
 using System.Threading;
 using UnityEngine;
 using VoidTemplate.Useful;
@@ -24,7 +22,8 @@ internal class Warp : UpdatableAndDeletable
 			new FloatField(timeToFadeOut, 0.1f, 200f, 60f, displayName: "fadeout time"),
 			new BooleanField(forceSpawningAtTarget, false, displayName: "force new den"),
 			new FloatField(cycleTime, 0f, 1f, 0f, 0.05f, displayName: "subtract time"),
-			new IntegerField(spendCycles, 0, 15, 0, displayName: "spend cycles")
+			new IntegerField(spendCycles, 0, 15, 0, displayName: "spend cycles"),
+			new BooleanField(exitInShortcut, false, displayName: "exit in shortcut"),
 			];
 		RegisterFullyManagedObjectType(exposedFields, typeof(Warp), category: "The Void");
 		WarpDestination.Register();
@@ -61,6 +60,13 @@ internal class Warp : UpdatableAndDeletable
 		}
 	}
 
+	class CustomLoader
+	{
+		public WorldLoader worldLoader;
+		public Warp warp;
+		public string targetRoom;
+	}
+
 	//Swapping the world directly from UAD yields a side effect:
 	//RWG
 	//for (int m = this.world.activeRooms.Count - 1; m >= 0; m--)
@@ -73,15 +79,15 @@ internal class Warp : UpdatableAndDeletable
 	//so when one swaps worlds from within UAD, update index stays
 	//but the number of active rooms drops
 	//which leads to index out of range
-	static ConditionalWeakTable<OverWorld, StrongBox<(WorldLoader, Warp, string)>> customLoader = new();
-private static void OverWorld_Update(On.OverWorld.orig_Update orig, OverWorld self)
+	static ConditionalWeakTable<OverWorld, CustomLoader> customLoader = new();
+	private static void OverWorld_Update(On.OverWorld.orig_Update orig, OverWorld self)
 	{
 		orig(self);
 		if(customLoader.TryGetValue(self, out var loader))
 		{
 
-			AbstractRoom absRoom = WorldLoaded(self, loader.Value);
-			loader.Value.Item2.OnRoomChange(absRoom);
+			AbstractRoom absRoom = WorldLoaded(self, loader);
+			loader.warp.OnRoomChange(absRoom);
 			customLoader.Remove(self);
 			GC.Collect();
 		}
@@ -90,87 +96,120 @@ private static void OverWorld_Update(On.OverWorld.orig_Update orig, OverWorld se
 		//if you dare to use this method, be aware that players realize at (10;10) coordinate
 		//it used to be (0;0), which really threw off camera logic
 		//after using this, change slugcat positions and apply camera change
-		AbstractRoom WorldLoaded(OverWorld overWorld, (WorldLoader, Warp, string) args)
+		AbstractRoom WorldLoaded(OverWorld overWorld, CustomLoader targetingData)
 		{
 			World world = overWorld.activeWorld;
-			World world2 = args.Item1.ReturnWorld();
-			AbstractRoom abstractRoom2 = null;
-			abstractRoom2 = world2.GetAbstractRoom(args.Item3);
-			overWorld.activeWorld = world2;
+			World newWorld = targetingData.worldLoader.ReturnWorld();
+			AbstractRoom targetAbstractRoom = null;
+			targetAbstractRoom = newWorld.GetAbstractRoom(targetingData.targetRoom);
+			overWorld.activeWorld = newWorld;
 			if (overWorld.game.roomRealizer != null)
 			{
-				overWorld.game.roomRealizer = new RoomRealizer(overWorld.game.roomRealizer.followCreature, world2);
+				overWorld.game.roomRealizer = new RoomRealizer(overWorld.game.roomRealizer.followCreature, newWorld);
 			}
 
-			abstractRoom2.RealizeRoom(world2, overWorld.game);
+			targetAbstractRoom.RealizeRoom(newWorld, overWorld.game);
+			WorldCoordinate newWorldCoordinate = new(targetAbstractRoom.index, 10, 10, -1);
 
 			foreach (AbstractCreature absPly in overWorld.game.Players)
 			{
-				WorldCoordinate worldCoordinate = new(abstractRoom2.index, 10, 10, -1);
-
 				absPly.world.GetAbstractRoom(absPly.pos).RemoveEntity(absPly);
-				absPly.world = world2;
-				absPly.pos = worldCoordinate;
-				absPly.world.GetAbstractRoom(worldCoordinate).AddEntity(absPly);
+				absPly.world = newWorld;
+				absPly.pos = newWorldCoordinate;
+				absPly.world.GetAbstractRoom(newWorldCoordinate).AddEntity(absPly);
+#nullable enable
+				ShortcutHandler.ShortCutVessel? containingVessel = overWorld.game.shortcuts.transportVessels.FirstOrDefault(x => x.creature == absPly.realizedCreature);
+#nullable disable
 
-
-				if (absPly.realizedCreature is Player p
-					&& p.room is not null)
+				switch (targetingData.warp.ExitInShortcut, containingVessel)
+				{
+					case (false, null): //when player is not in shortcut, as intended
+						break;
+					case (false, not null): //when player is in shortcut and should not be
+						containingVessel.room = targetAbstractRoom;
+						containingVessel.pos = new(newWorldCoordinate.x, newWorldCoordinate.y);
+						overWorld.game.shortcuts.SpitOutCreature(containingVessel);
+						break;
+					case (true, null): //when player is not in shortcut and should be
+						overWorld.game.shortcuts.transportVessels.Add(
+							new ShortcutHandler.ShortCutVessel(new(newWorldCoordinate.x, newWorldCoordinate.y), absPly.realizedCreature, targetAbstractRoom, 0)
+							);
+						absPly.realizedCreature.inShortcut = true;
+						break;
+					case (true, not null): //when player is in shortcut, as intended
+						break;
+				}
+				if (!targetingData.warp.ExitInShortcut
+					&& absPly.realizedCreature is Player p
+						   && p.room is not null)
 				{
 					p.room.RemoveObject(p);
-					p.PlaceInRoom(abstractRoom2.realizedRoom);
+					p.PlaceInRoom(targetAbstractRoom.realizedRoom);
 					p.standing = true;
-					
-					if (p.objectInStomach is not null) p.objectInStomach.world = world2;
+
+					if (p.objectInStomach is not null) p.objectInStomach.world = newWorld;
 
 					foreach (Creature.Grasp grasp in p.grasps)
 					{
-						if(grasp is not null)
+						if (grasp is not null)
 						{
 							var APO = grasp.grabbed.abstractPhysicalObject;
 							APO.world.GetAbstractRoom(APO.pos).RemoveEntity(APO);
-							APO.world = world2;
-							APO.pos = worldCoordinate;
-							APO.world.GetAbstractRoom (worldCoordinate).AddEntity(APO);
+							APO.world = newWorld;
+							APO.pos = newWorldCoordinate;
+							APO.world.GetAbstractRoom(newWorldCoordinate).AddEntity(APO);
 						}
 					}
 				}
+				else if(targetingData.warp.ExitInShortcut)
+				{
+					//the attempt to guess where to put the vessel is way too expensive. i'll just rely on wDestination
+                }
 			}
-			Room realizedDestination = abstractRoom2.realizedRoom;
-			WarpDestination warpDestination = null;
-			if (realizedDestination.updateList.Exists(UAD =>
+			Room realizedDestination = targetAbstractRoom.realizedRoom;
+			if (realizedDestination.updateList.OfType<WarpDestination>().FirstOrDefault() is WarpDestination warpDestination)
 			{
-				if (UAD is WarpDestination wDest)
+                List<AbstractCreature> players = realizedDestination.game.Players;
+                if (targetingData.warp.ExitInShortcut)
 				{
-					warpDestination = wDest;
-					return true;
-				}
-				return false;
-			}))
-			{
-				List<AbstractCreature> players = realizedDestination.game.Players;
-				for (short i = 0; i < players.Count; i++)
-				{
-					if (players[i].realizedCreature is not null)
+					RWCustom.IntVector2 pos = realizedDestination.GetTilePosition(warpDestination.Pos);
+                    IEnumerable<ShortcutHandler.ShortCutVessel> playerVessels = overWorld.game.shortcuts.transportVessels.Where(vessel => realizedDestination.game.Players.Any(absply => vessel.creature == absply.realizedCreature));
+					foreach(var playerVessel in playerVessels)
 					{
-						foreach (var bodyChunk in players[i].realizedCreature.bodyChunks)
-						{
-							bodyChunk.pos = warpDestination.Pos + new Vector2(i * 20f, 0f);
-						}
+						playerVessel.pos = pos;
+						playerVessel.lastPositions[0] = pos with { y = pos.y + 1 };
 					}
 				}
+				else
+				{
+                    
+                    for (short i = 0; i < players.Count; i++)
+                    {
+                        if (players[i].realizedCreature is not null)
+                        {
+                            foreach (var bodyChunk in players[i].realizedCreature.bodyChunks)
+                            {
+                                bodyChunk.pos = warpDestination.Pos + new Vector2(i * 20f, 0f);
+                            }
+                        }
+                    }
+                }
 			}
+			else if (targetingData.warp.ExitInShortcut) 
+				throw new EntryPointNotFoundException($"failed to locate exit point for Last Wish warp to {targetingData.targetRoom} while requesting it to be in shortcut");
+
+
 
 			foreach (var camera in overWorld.game.cameras)
 			{
 				camera.virtualMicrophone.AllQuiet();
 
-				camera.MoveCamera(abstractRoom2.realizedRoom, 0);
+				camera.MoveCamera(targetAbstractRoom.realizedRoom, 0);
 				camera.ApplyPositionChange();
 				camera.GetCameraBestIndex();
 
 				var hud = camera.hud;
-				hud.ResetMap(new(world2, overWorld.game.rainWorld));
+				hud.ResetMap(new(newWorld, overWorld.game.rainWorld));
 				camera.dayNightNeedsRefresh = true;
 				if (hud.textPrompt.subregionTracker is not null) hud.textPrompt.subregionTracker.lastShownRegion = 0;
 
@@ -180,34 +219,34 @@ private static void OverWorld_Update(On.OverWorld.orig_Update orig, OverWorld se
 
 			if (world.regionState is not null) world.regionState.world = null;
 
-			world2.rainCycle.baseCycleLength = world.rainCycle.baseCycleLength;
-			world2.rainCycle.cycleLength = world.rainCycle.cycleLength;
-			world2.rainCycle.timer = world.rainCycle.timer;
-			world2.rainCycle.duskPalette = world.rainCycle.duskPalette;
-			world2.rainCycle.nightPalette = world.rainCycle.nightPalette;
-			world2.rainCycle.dayNightCounter = world.rainCycle.dayNightCounter;
+			newWorld.rainCycle.baseCycleLength = world.rainCycle.baseCycleLength;
+			newWorld.rainCycle.cycleLength = world.rainCycle.cycleLength;
+			newWorld.rainCycle.timer = world.rainCycle.timer;
+			newWorld.rainCycle.duskPalette = world.rainCycle.duskPalette;
+			newWorld.rainCycle.nightPalette = world.rainCycle.nightPalette;
+			newWorld.rainCycle.dayNightCounter = world.rainCycle.dayNightCounter;
 
 			if (ModManager.MSC)
 			{
 				if (world.rainCycle.timer == 0)
 				{
-					world2.rainCycle.preTimer = world.rainCycle.preTimer;
-					world2.rainCycle.maxPreTimer = world.rainCycle.maxPreTimer;
+					newWorld.rainCycle.preTimer = world.rainCycle.preTimer;
+					newWorld.rainCycle.maxPreTimer = world.rainCycle.maxPreTimer;
 				}
 				else
 				{
-					world2.rainCycle.preTimer = 0;
-					world2.rainCycle.maxPreTimer = 0;
+					newWorld.rainCycle.preTimer = 0;
+					newWorld.rainCycle.maxPreTimer = 0;
 				}
 			}
 
-			if (!world2.activeRooms.Contains(abstractRoom2.realizedRoom)) world2.activeRooms.Add(abstractRoom2.realizedRoom);
+			if (!newWorld.activeRooms.Contains(targetAbstractRoom.realizedRoom)) newWorld.activeRooms.Add(targetAbstractRoom.realizedRoom);
 
 			if (ModManager.MMF)
 			{
 				GC.Collect();
 			}
-			return abstractRoom2;
+			return targetAbstractRoom;
 		}
 	}
 
@@ -219,37 +258,41 @@ private static void OverWorld_Update(On.OverWorld.orig_Update orig, OverWorld se
 	const string forceSpawningAtTarget = "forceSpawning";
 	const string cycleTime = "cycleTime";
 	const string spendCycles = "spendCycles";
+	const string exitInShortcut = "exitInShortcut";
 	#endregion
 
 	public Warp(Room room, PlacedObject pobj)
 	{
 		this.room = room;
 		placedObject = pobj;
-		data = placedObject.data as ManagedData;
+		data = (placedObject.data as ManagedData)!;
 	}
 
 	#region exposed fields
 	Vector2[] TriggerZone => POMUtils.AddRealPosition(data.GetValue<Vector2[]>(triggerZone), placedObject.pos);
-	string TargetRoom => data.GetValue<string>(targetRoomName);
+	string TargetRoom => data.GetValue<string>(targetRoomName)!;
 	string Acronym => TargetRoom.Split('_')[0];
 	float TimeToFadeIn => data.GetValue<float>(timeToFadeIn);
 	float TimeToFadeOut => data.GetValue<float>(timeToFadeOut);
 	bool ForceDenSwitch => data.GetValue<bool>(forceSpawningAtTarget);
 	float SubtractCycleTime => data.GetValue<float>(cycleTime);
 	int SubtractCycles => data.GetValue<int>(spendCycles);
+	bool ExitInShortcut => data.GetValue<bool>(exitInShortcut);
 	#endregion
 
 
 	#region runtime variables
+#nullable enable
 	readonly PlacedObject placedObject;
 	readonly ManagedData data;
-	private FadeOut fadeOut;
+	private FadeOut? fadeOut;
 	private State state = State.awaiting;
 	//vanilla worldloading happens during a few ticks, and then immediately changes world
 	//so i want to load world prematurely and only utilize it afterwards
-	private WorldLoader worldLoader;
-	private Thread thread;
-	private ThreadedLoading threadedLoading;
+	private WorldLoader? worldLoader;
+	private Thread? thread;
+	private ThreadedLoading? threadedLoading;
+#nullable disable
 
 	enum State
 	{
@@ -258,15 +301,21 @@ private static void OverWorld_Update(On.OverWorld.orig_Update orig, OverWorld se
 		awaitingWorld,
 		awaitingTransition
 	}
-    #endregion
-    public override void Update(bool eu)
+	#endregion
+	public override void Update(bool eu)
 	{
 		base.Update(eu);
 		switch (state)
 		{
 			case State.awaiting:
-				if (room.PlayersInRoom.Any(realizedPlayerInRoom => realizedPlayerInRoom is not null
-				&& PositionWithinPoly(TriggerZone, realizedPlayerInRoom.mainBodyChunk.pos)))
+				if (room.PlayersInRoom.Any(
+					realizedPlayerInRoom => 
+						realizedPlayerInRoom is not null
+						&& PositionWithinPoly(TriggerZone, realizedPlayerInRoom.mainBodyChunk.pos))
+					|| room.game.shortcuts.transportVessels.Any( 
+						vessel => 
+						vessel.room == room.abstractRoom 
+						&& PositionWithinPoly(TriggerZone, room.MiddleOfTile(vessel.pos))))
 				{
 					room.game.cameras[0].EnterCutsceneMode(room.PlayersInRoom[0].abstractCreature, RoomCamera.CameraCutsceneType.EndingOE);
 					fadeOut = new FadeOut(room, Color.black, duration: TimeToFadeIn, fadeIn: false);
@@ -290,7 +339,12 @@ private static void OverWorld_Update(On.OverWorld.orig_Update orig, OverWorld se
 					&& worldLoader.ReturnWorld() is not null)
 				{
 					OverWorld overWorld = room.game.overWorld;
-					customLoader.Add(overWorld, new((worldLoader, this, TargetRoom)));
+					customLoader.Add(overWorld, new CustomLoader()
+					{
+						worldLoader = worldLoader,
+						warp = this,
+						targetRoom = TargetRoom
+					});
 					worldLoader = null;
 					state = State.awaitingTransition;
 				}
@@ -334,16 +388,16 @@ private static void OverWorld_Update(On.OverWorld.orig_Update orig, OverWorld se
 		private readonly Warp warp = warp;
 		private readonly string acronym = targetRegionAcronym;
 
-        public void Load()
+		public void Load()
 		{
 			WorldLoader worldLoader = new(game: room.game,
-                        playerCharacter: room.game.GetStorySession.characterStats.name,
-                        timelinePosition: SlugcatStats.SlugcatToTimeline(room.game.StoryCharacter),
+						playerCharacter: room.game.GetStorySession.characterStats.name,
+						timelinePosition: SlugcatStats.SlugcatToTimeline(room.game.StoryCharacter),
 						singleRoomWorld: false,
 						//this may be wrong, maybe there is no need to wrap
 						worldName: Region.GetProperRegionAcronym(SlugcatStats.SlugcatToTimeline(room.game.StoryCharacter), acronym),
 
-                        region: room.game.overWorld.GetRegion(acronym),
+						region: room.game.overWorld.GetRegion(acronym),
 						setupValues: room.game.setupValues);
 			worldLoader.NextActivity();
 			while (!worldLoader.Finished)

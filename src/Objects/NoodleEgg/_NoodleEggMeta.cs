80bb9e6ce7871e81b69d5bae967e5c34cc36e4fb
Liebe Asano <liebeasano@gmail.com>
2025-06-08 15:15:29 +0300

    IL NeedleEggs LogErr

--- src/Objects/NoodleEgg/_NoodleEggMeta.cs
+++ src/Objects/NoodleEgg/_NoodleEggMeta.cs
@@ -3,6 +3,7 @@ using Mono.Cecil.Cil;
 using MoreSlugcats;
 using static VoidTemplate.Useful.Utils;
 using System;
+using VoidTemplate.PlayerMechanics;
 
 namespace VoidTemplate.Objects.NoodleEgg
 {
@@ -17,54 +18,71 @@ namespace VoidTemplate.Objects.NoodleEgg
         private static void SlugcatHand_Update(ILContext il)
         {
             ILCursor c = new(il);
-            c.GotoNext(x => x.MatchLdfld<Player>("eatCounter"));
-            c.GotoNext(MoveType.After, x => x.MatchBrfalse(out _));
-
-            ILCursor u = c.Clone();
-            u.GotoNext(MoveType.Before,
-                x => x.MatchLdloc(4),
-                x => x.MatchStloc(3));
-            ILLabel label = u.MarkLabel();
-
-            c.Emit(OpCodes.Ldarg_0);
-            c.Emit(OpCodes.Ldloc, 4);
-            c.EmitDelegate((SlugcatHand hand, int grasp) =>
+            if (c.TryGotoNext(x => x.MatchLdfld<Player>("eatCounter"))
+                && c.TryGotoNext(MoveType.After, x => x.MatchBrfalse(out _)))
             {
-                if ((hand.owner.owner as Player).grasps[grasp].grabbed is NeedleEgg)
+                ILCursor u = c.Clone();
+                if (u.TryGotoNext(MoveType.Before,
+                    x => x.MatchLdloc(4),
+                    x => x.MatchStloc(3)))
                 {
-                    return true;
+                    ILLabel label = u.MarkLabel();
+
+                    c.Emit(OpCodes.Ldarg_0);
+                    c.Emit(OpCodes.Ldloc, 4);
+                    c.EmitDelegate((SlugcatHand hand, int grasp) =>
+                    {
+                        if ((hand.owner.owner as Player).grasps[grasp].grabbed is NeedleEgg)
+                        {
+                            return true;
+                        }
+                        return false;
+                    });
+                    c.Emit(OpCodes.Brtrue, label);
                 }
-                return false;
-            });
-            c.Emit(OpCodes.Brtrue, label);
+                else
+                {
+                    logerr($"{nameof(VoidTemplate.Objects.NoodleEgg)}.{nameof(_NoodleEggMeta)}.{nameof(SlugcatHand_Update)}: second match failed");
+                }
+            }
+            else
+            {
+                logerr($"{nameof(VoidTemplate.Objects.NoodleEgg)}.{nameof(_NoodleEggMeta)}.{nameof(SlugcatHand_Update)}: first match failed");
+            }
         }
 
         private static void Player_BiteEdibleObject(ILContext il)
         {
             ILCursor c = new(il);
 
-            c.GotoNext(MoveType.After, x => x.MatchBrfalse(out _));
-
-            ILLabel label = c.Clone().MarkLabel();
-
-            c.Emit(OpCodes.Ldarg_0);
-            c.Emit(OpCodes.Ldarg_1);
-            c.Emit(OpCodes.Ldloc_0);
-            c.EmitDelegate((Player player, bool eu, int grasp) =>
+            if (c.TryGotoNext(MoveType.After, x => x.MatchBrfalse(out _)))
             {
-                if (player.AreVoidViy() && player.grasps[grasp].grabbed is NeedleEgg egg)
+
+                ILLabel label = c.Clone().MarkLabel();
+
+                c.Emit(OpCodes.Ldarg_0);
+                c.Emit(OpCodes.Ldarg_1);
+                c.Emit(OpCodes.Ldloc_0);
+                c.EmitDelegate((Player player, bool eu, int grasp) =>
                 {
-                    if (player.graphicsModule != null)
+                    if (player.AreVoidViy() && player.grasps[grasp].grabbed is NeedleEgg egg)
                     {
-                        (player.graphicsModule as PlayerGraphics).BiteFly(grasp);
+                        if (player.graphicsModule != null)
+                        {
+                            (player.graphicsModule as PlayerGraphics).BiteFly(grasp);
+                        }
+                        egg.GetEdible().Bite(player.grasps[grasp], eu);
+                        return true;
                     }
-                    egg.GetEdible().Bite(player.grasps[grasp], eu);
-                    return true;
-                }
-                return false;
-            });
-            c.Emit(OpCodes.Brfalse, label);
-            c.Emit(OpCodes.Ret);
+                    return false;
+                });
+                c.Emit(OpCodes.Brfalse, label);
+                c.Emit(OpCodes.Ret);
+            }
+            else
+            {
+                logerr($"{nameof(VoidTemplate.Objects.NoodleEgg)}.{nameof(_NoodleEggMeta)}.{nameof(Player_BiteEdibleObject)}: first match failed");
+            }
         }
     }
 }

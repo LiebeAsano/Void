a8b8768e71433d10c32ee96d765855e66b60286c
Liebe Asano <liebeasano@gmail.com>
2025-07-05 22:20:20 +0300

    Please Viy Back

--- src/PlayerMechanics/ViyMechanics/VoidViySwitch.cs
+++ src/PlayerMechanics/ViyMechanics/VoidViySwitch.cs
@@ -26,6 +26,11 @@ public static class VoidViySwitch
         int indexOfHunter = self.slugcatColorOrder.IndexOf(SlugcatStats.Name.Red);
         if (indexOfHunter >= 0)
         {
+            if (voidDead && voidKarma11)
+            {
+                self.slugcatColorOrder.Insert(indexOfHunter + 1, VoidEnums.SlugcatID.Viy);
+            }
+            else
             {
                 self.slugcatColorOrder.Insert(indexOfHunter + 1, VoidEnums.SlugcatID.Void);
             }

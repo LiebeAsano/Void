6688fa64fc2ce171972ad82da7c19101e34658c7
Liebe Asano <liebeasano@gmail.com>
2025-07-11 21:07:43 +0300

    Absolute Optimization x4

--- src/PlayerMechanics/BasiliskResist.cs
+++ src/PlayerMechanics/BasiliskResist.cs
@@ -3,24 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VoidTemplate.Useful;
 
-namespace VoidTemplate.PlayerMechanics
+namespace VoidTemplate.PlayerMechanics;
+
+public class BasiliskResist
 {
-    public class BasiliskResist
-    {
-        public static void Hook()
-        {
-            On.Player.Update += Player_Update;
-        }
+    public static void Hook() => On.Player.Update += Player_Update;
 
-        private static void Player_Update(On.Player.orig_Update orig, Player self, bool eu)
+    private static void Player_Update(On.Player.orig_Update orig, Player self, bool eu)
+    {
+        if ((self.injectedPoison > 0 || self.mushroomCounter > 0) &&
+            self.AreVoidViy() &&
+            !self.chatlog)
         {
-            if (self.injectedPoison > 0 || self.mushroomCounter > 0 && (self.slugcatStats.name == VoidEnums.SlugcatID.Void || self.slugcatStats.name != VoidEnums.SlugcatID.Viy) && self.chatlog == false)
-            {
-                self.mushroomCounter = 0;
-                self.injectedPoison = 0;
-            }
-            orig(self, eu);
+            self.mushroomCounter = self.injectedPoison = 0;
         }
+        orig(self, eu);
     }
 }

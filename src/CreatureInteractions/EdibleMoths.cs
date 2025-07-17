427806445d7691b7bcd46f972e5e983968fa7bc4
Quansly <168752094+Quansly@users.noreply.github.com>
2025-07-18 01:01:59 +0700

    Bright smaill moths

--- src/CreatureInteractions/EdibleMoths.cs
+++ src/CreatureInteractions/EdibleMoths.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace VoidTemplate.CreatureInteractions
 {
@@ -11,6 +12,24 @@ namespace VoidTemplate.CreatureInteractions
         public static void Hook()
         {
             On.StaticWorld.InitSmallMoth += StaticWorld_InitSmallMoth;
+            On.Watcher.BigMoth.GenerateIVars += BigMoth_GenerateIVars;
+        }
+
+        private static void BigMoth_GenerateIVars(On.Watcher.BigMoth.orig_GenerateIVars orig, Watcher.BigMoth self)
+        {
+            orig(self);
+            if (self.Small && !Region.IsAncientUrbanRegion(self.abstractCreature.world.name))
+            {
+                UnityEngine.Random.State state = UnityEngine.Random.state;
+                UnityEngine.Random.InitState(self.abstractCreature.ID.RandomSeed);
+                Color b = Color.Lerp(new Color(0.5f, 0.25f, 0.1f), new Color(0.6f, 0.5f, 0.4f), UnityEngine.Random.value);
+                Color color = Color.Lerp(Color.white, b, Mathf.Pow(UnityEngine.Random.value, 2f) * 0.17f);
+                color = Color.Lerp(color, new Color(1f, 0.7f, 0f), (self.abstractCreature.personality.dominance - Mathf.Pow(UnityEngine.Random.value, 2f)) * 0.1f);
+                Color secondaryColor = Color.Lerp(Color.Lerp(color, new HSLColor(UnityEngine.Random.value * 0.17f, 1f, 0.5f).rgb, UnityEngine.Random.value * 0.4f), Color.black, 0.3f);
+                self.iVars.bodyColor = color;
+                self.iVars.secondaryColor = secondaryColor;
+                UnityEngine.Random.state = state;
+            }
         }
 
         private static void StaticWorld_InitSmallMoth(On.StaticWorld.orig_InitSmallMoth orig, List<CreatureTemplate> tempCreatureTemplates, CreatureTemplate bigMothTemplate, CreatureTemplate batTemplate)

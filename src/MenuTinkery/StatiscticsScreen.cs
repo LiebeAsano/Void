8dbcfd2393244c0d8532f447a15efabc2b83fbf0
Liebe Asano <liebeasano@gmail.com>
2025-08-12 15:20:12 +0300

    Ice Lizard Fix

--- src/MenuTinkery/StatiscticsScreen.cs
+++ src/MenuTinkery/StatiscticsScreen.cs
@@ -8,33 +8,32 @@ using Menu;
 using Mono.Cecil.Cil;
 using static VoidTemplate.Useful.Utils;
 
-namespace VoidTemplate.MenuTinkery
-{
-    public class StatiscticsScreen
-    {
-        public static void Hook()
-        {
-            IL.Menu.StoryGameStatisticsScreen.TickerIsDone += StoryGameStatisticsScreen_TickerIsDone;
-        }
+namespace VoidTemplate.MenuTinkery;
 
-        private static void StoryGameStatisticsScreen_TickerIsDone(ILContext il)
+public class StatiscticsScreen
+{
+    public static void Hook()
+    {
+        IL.Menu.StoryGameStatisticsScreen.TickerIsDone += StoryGameStatisticsScreen_TickerIsDone;
+    }
+
+    private static void StoryGameStatisticsScreen_TickerIsDone(ILContext il)
+    {
+        ILCursor c = new(il);
+        if (c.TryGotoNext(MoveType.After,
+            x => x.MatchCall<StoryGameStatisticsScreen>(nameof(StoryGameStatisticsScreen.GetNonSandboxKillscore)),
+            x => x.MatchStloc(1),
+            x => x.MatchLdloc(1)))
         {
-            ILCursor c = new(il);
-            if (c.TryGotoNext(MoveType.After,
-                x => x.MatchCall<StoryGameStatisticsScreen>(nameof(StoryGameStatisticsScreen.GetNonSandboxKillscore)),
-                x => x.MatchStloc(1),
-                x => x.MatchLdloc(1)))
+            c.Emit(OpCodes.Ldloc_0);
+            c.EmitDelegate((int origNum, IconSymbol.IconSymbolData iconData) =>
             {
-                c.Emit(OpCodes.Ldloc_0);
-                c.EmitDelegate((int origNum, IconSymbol.IconSymbolData iconData) =>
-                {
-                    return origNum != 0 || MultiplayerUnlocks.SandboxUnlockForSymbolData(iconData).Index < 0;
-                });
-            }
-            else
-            {
-                logerr($"{nameof(MenuTinkery)}.{nameof(StatiscticsScreen)}.{nameof(StoryGameStatisticsScreen_TickerIsDone)}: match error.");
-            }
+                return origNum != 0 || MultiplayerUnlocks.SandboxUnlockForSymbolData(iconData).Index < 0;
+            });
+        }
+        else
+        {
+            logerr($"{nameof(MenuTinkery)}.{nameof(StatiscticsScreen)}.{nameof(StoryGameStatisticsScreen_TickerIsDone)}: match error.");
         }
     }
 }

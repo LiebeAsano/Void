732bde0efdf1a1dd09a19fe373950e1d8726f101
Quansly <168752094+Quansly@users.noreply.github.com>
2025-10-19 22:10:21 +0700

    Remove contorl tip image and text

--- src/MenuTinkery/PauseMenuTips.cs
+++ src/MenuTinkery/PauseMenuTips.cs
@@ -17,15 +17,33 @@ public class PauseMenuTips
         On.Menu.ControlMap.ctor += ControlMap_ctor;
     }
 
-    private static void ControlMap_ctor(On.Menu.ControlMap.orig_ctor orig, ControlMap self, Menu.Menu menu, MenuObject owner, UnityEngine.Vector2 pos, Options.ControlSetup.Preset preset, bool showPickupInstructions)
+    private static void ControlMap_ctor(On.Menu.ControlMap.orig_ctor orig, ControlMap self, Menu.Menu menu, MenuObject owner, Vector2 pos, Options.ControlSetup.Preset preset, bool showPickupInstructions)
     {
         orig(self, menu, owner, pos, preset, showPickupInstructions);
         if (menu is PauseMenu pause && pause.game.IsVoidWorld())
         {
-            self.controlLabels = [];
-            self.controlsMap = new MenuIllustration(menu, self, "", "", new Vector2(0f, 0f), true, false);
-            self.controlsMap2 = new MenuIllustration(menu, self, "", "", self.controlsMap.pos + new Vector2(0f, 0f), true, false);
-            self.controlsMap3 = new MenuIllustration(menu, self, "", "", self.controlsMap.pos + new Vector2(0f, 0f), true, false);
+            //self.controlLabels = [];
+            //self.controlsMap = new MenuIllustration(menu, self, "", "", new Vector2(0f, 0f), true, false);            
+            //self.controlsMap2 = new MenuIllustration(menu, self, "", "", self.controlsMap.pos + new Vector2(0f, 0f), true, false);
+            //self.controlsMap3 = new MenuIllustration(menu, self, "", "", self.controlsMap.pos + new Vector2(0f, 0f), true, false);
+            if (self.controlsMap != null)
+            {
+                self.controlsMap.RemoveSprites();
+                self.RemoveSubObject(self.controlsMap);
+                self.controlsMap = null;
+                self.controlsMap2.RemoveSprites();
+                self.RemoveSubObject(self.controlsMap2);
+                self.controlsMap2 = null;
+                self.controlsMap3.RemoveSprites();
+                self.RemoveSubObject(self.controlsMap3);
+                self.controlsMap3 = null;
+                for (int i = 0; i < self.controlLabels.Length; i++)
+                {
+                    self.controlLabels[i].RemoveSprites();
+                    self.RemoveSubObject(self.controlLabels[i]);
+                    self.controlLabels[i].label.text = "";
+                }
+            }
             self.pickupButtonInstructions.text = $"{menu.Translate("Pick up / Eat / Maul button interactions:") + "\r\n\r\n"}";
             self.pickupButtonInstructions.text += "  - " + menu.Translate("Tap to pick up object") + "\r\n";
             self.pickupButtonInstructions.text += "  - " + menu.Translate("Hold to eat / swallow object / maul stunned creature") + "\r\n";

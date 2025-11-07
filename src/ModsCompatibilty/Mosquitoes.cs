a7dfe161d81927e82770f693ee5ffc13486d7bc2
Liebe Asano <liebeasano@gmail.com>
2025-11-07 18:22:21 +0300

    Mosquitoes Fix Prime

--- src/ModsCompatibilty/Mosquitoes.cs
+++ src/ModsCompatibilty/Mosquitoes.cs
@@ -12,11 +12,11 @@ namespace VoidTemplate.ModsCompatibilty
 {
     public class MosquitoCompat
     {
-
         private static readonly ConditionalWeakTable<object, VoidInfection> infectedMosquitoes = new();
 
         public static void Init()
         {
+
             Assembly mosquitoAssembly = AppDomain.CurrentDomain.GetAssemblies()
                 .FirstOrDefault(asm => asm.GetName().Name == "Mosquitoes");
 
@@ -33,6 +33,12 @@ namespace VoidTemplate.ModsCompatibilty
                 return;
             }
 
+            InitHooks(mosquitoType, mosquitoGraphicsType);
+
+        }
+
+        private static void InitHooks(Type mosquitoType, Type mosquitoGraphicsType)
+        {
             MethodInfo stickIntoChunkMethod = mosquitoType.GetMethod("StickIntoChunk",
                 BindingFlags.Public | BindingFlags.Instance,
                 null,
@@ -76,11 +82,10 @@ namespace VoidTemplate.ModsCompatibilty
             {
                 new Hook(bitByPlayerMethod, BitByPlayerHook);
             }
-
         }
 
         private static readonly Delegate StickIntoChunkHook =
-            (Action<Mosquitoes.Mosquito, PhysicalObject, int> orig, Mosquitoes.Mosquito self, PhysicalObject otherObject, int otherChunk) =>
+            (Action<Mosquito, PhysicalObject, int> orig, Mosquito self, PhysicalObject otherObject, int otherChunk) =>
             {
                 if (otherObject is Player player && player.IsVoid())
                 {
@@ -91,7 +96,7 @@ namespace VoidTemplate.ModsCompatibilty
             };
 
         private static readonly Delegate UpdateHook =
-            (Action<Mosquitoes.Mosquito, bool> orig, Mosquitoes.Mosquito self, bool eu) =>
+            (Action<Mosquito, bool> orig, Mosquito self, bool eu) =>
             {
                 orig(self, eu);
 
@@ -99,9 +104,13 @@ namespace VoidTemplate.ModsCompatibilty
                 {
                     infection.timer++;
 
-                    if (infection.timer >= 240 && !self.dead)
+                    var deadProperty = self.GetType().GetProperty("dead");
+                    bool isDead = deadProperty != null && (bool)deadProperty.GetValue(self);
+
+                    if (infection.timer >= 240 && !isDead)
                     {
-                        self.Die();
+                        var dieMethod = self.GetType().GetMethod("Die");
+                        dieMethod?.Invoke(self, null);
 
                         if (infectedMosquitoes.TryGetValue(self, out var deadInfection))
                         {
@@ -112,49 +121,67 @@ namespace VoidTemplate.ModsCompatibilty
             };
 
         private static readonly Delegate ApplyPaletteHook =
-            (Action<Mosquitoes.MosquitoGraphics, RoomCamera.SpriteLeaser, RoomCamera, RoomPalette> orig, Mosquitoes.MosquitoGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, RoomPalette palette) =>
+            (Action<MosquitoGraphics, RoomCamera.SpriteLeaser, RoomCamera, RoomPalette> orig, MosquitoGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, RoomPalette palette) =>
             {
                 orig(self, sLeaser, rCam, palette);
 
-                if (infectedMosquitoes.TryGetValue(self.bug, out var infection))
+                var bugField = self.GetType().GetField("bug");
+                if (bugField == null) return;
+
+                var mosquito = bugField.GetValue(self);
+                if (mosquito == null) return;
+
+                if (infectedMosquitoes.TryGetValue(mosquito, out var infection))
                 {
                     float progress = infection.timer / 240f;
 
-                    Color currentMainColor = (sLeaser.sprites[0] as TriangleMesh).verticeColors[2];
-                    Color currentAccentColor = (sLeaser.sprites[0] as TriangleMesh).verticeColors[(sLeaser.sprites[0] as TriangleMesh).verticeColors.Length - 1];
-                    Color currentWhiteColor = (sLeaser.sprites[0] as TriangleMesh).verticeColors[0];
-
-                    Color voidColor = Color.Lerp(currentMainColor, DrawSprites.voidColor, progress);
-                    Color voidAccent = Color.Lerp(currentAccentColor, DrawSprites.voidColor, progress);
-                    Color voidWhite = Color.Lerp(currentWhiteColor, DrawSprites.voidColor, progress);
-
-                    for (int i = 0; i < (sLeaser.sprites[0] as TriangleMesh).verticeColors.Length; i++)
+                    if (sLeaser.sprites[0] is TriangleMesh mesh)
                     {
-                        float value = Mathf.InverseLerp(0f, (float)((sLeaser.sprites[0] as TriangleMesh).verticeColors.Length - 1), (float)i);
-                        (sLeaser.sprites[0] as TriangleMesh).verticeColors[i] = Color.Lerp(voidColor, voidAccent, 0.25f + Mathf.InverseLerp(0f, 1f, value) * 0.75f * self.bug.bloat);
-                    }
+                        Color currentMainColor = mesh.verticeColors[2];
+                        Color currentAccentColor = mesh.verticeColors[mesh.verticeColors.Length - 1];
+                        Color currentWhiteColor = mesh.verticeColors[0];
 
-                    (sLeaser.sprites[0] as TriangleMesh).verticeColors[0] = voidWhite;
-                    (sLeaser.sprites[0] as TriangleMesh).verticeColors[1] = voidWhite;
+                        Color voidColor = Color.Lerp(currentMainColor, DrawSprites.voidColor, progress);
+                        Color voidAccent = Color.Lerp(currentAccentColor, DrawSprites.voidColor, progress);
+                        Color voidWhite = Color.Lerp(currentWhiteColor, DrawSprites.voidColor, progress);
 
-                    for (int j = 0; j < (sLeaser.sprites[1] as TriangleMesh).verticeColors.Length; j++)
-                    {
-                        float value2 = Mathf.InverseLerp(0f, (float)((sLeaser.sprites[1] as TriangleMesh).verticeColors.Length - 1), (float)j);
-                        (sLeaser.sprites[1] as TriangleMesh).verticeColors[j] = Custom.RGB2RGBA(voidWhite, Mathf.InverseLerp(0f, 0.2f, value2) * 0f);
-                    }
+                        for (int i = 0; i < mesh.verticeColors.Length; i++)
+                        {
+                            float value = Mathf.InverseLerp(0f, mesh.verticeColors.Length - 1, i);
+                            var bloatProperty = mosquito.GetType().GetProperty("bloat");
+                            float bloat = bloatProperty != null ? (float)bloatProperty.GetValue(mosquito) : 1f;
+                            mesh.verticeColors[i] = Color.Lerp(voidColor, voidAccent, 0.25f + Mathf.InverseLerp(0f, 1f, value) * 0.75f * bloat);
+                        }
 
-                    for (int k = 0; k < 2; k++)
-                    {
-                        (sLeaser.sprites[2 + k] as CustomFSprite).verticeColors[2] = Color.Lerp(palette.fogColor, voidColor, 0.5f);
-                        (sLeaser.sprites[2 + k] as CustomFSprite).verticeColors[3] = Color.Lerp(palette.fogColor, voidColor, 0.5f);
-                        (sLeaser.sprites[2 + k] as CustomFSprite).verticeColors[0] = Color.Lerp(palette.fogColor, voidWhite, 0.5f);
-                        (sLeaser.sprites[2 + k] as CustomFSprite).verticeColors[1] = Color.Lerp(palette.fogColor, voidWhite, 0.5f);
+                        mesh.verticeColors[0] = voidWhite;
+                        mesh.verticeColors[1] = voidWhite;
+
+
+                        if (sLeaser.sprites[1] is TriangleMesh mesh1)
+                        {
+                            for (int j = 0; j < mesh1.verticeColors.Length; j++)
+                            {
+                                float value2 = Mathf.InverseLerp(0f, mesh1.verticeColors.Length - 1, j);
+                                mesh1.verticeColors[j] = Custom.RGB2RGBA(voidWhite, Mathf.InverseLerp(0f, 0.2f, value2) * 0f);
+                            }
+                        }
+
+                        for (int k = 0; k < 2; k++)
+                        {
+                            if (sLeaser.sprites[2 + k] is CustomFSprite wingSprite)
+                            {
+                                wingSprite.verticeColors[2] = Color.Lerp(palette.fogColor, voidColor, 0.5f);
+                                wingSprite.verticeColors[3] = Color.Lerp(palette.fogColor, voidColor, 0.5f);
+                                wingSprite.verticeColors[0] = Color.Lerp(palette.fogColor, voidWhite, 0.5f);
+                                wingSprite.verticeColors[1] = Color.Lerp(palette.fogColor, voidWhite, 0.5f);
+                            }
+                        }
                     }
                 }
             };
 
         private static readonly Delegate BitByPlayerHook =
-            (Action<Mosquitoes.Mosquito, Creature.Grasp, bool> orig, Mosquitoes.Mosquito self, Creature.Grasp grasp, bool eu) =>
+            (Action<Mosquito, Creature.Grasp, bool> orig, Mosquito self, Creature.Grasp grasp, bool eu) =>
             {
                 bool wasInfected = infectedMosquitoes.TryGetValue(self, out var infection);
                 bool isLethal = wasInfected && infection.lethalToNonVoid;
@@ -172,11 +199,15 @@ namespace VoidTemplate.ModsCompatibilty
                 }
             };
 
-        private static void InfectMosquito(Mosquitoes.Mosquito mosquito)
+        private static void InfectMosquito(object mosquito)
         {
-            if (!infectedMosquitoes.TryGetValue(mosquito, out _) && !mosquito.dead)
+            if (!infectedMosquitoes.TryGetValue(mosquito, out _))
             {
-                infectedMosquitoes.Add(mosquito, new VoidInfection());
+                var deadProperty = mosquito.GetType().GetProperty("dead");
+                if (deadProperty != null && !(bool)deadProperty.GetValue(mosquito))
+                {
+                    infectedMosquitoes.Add(mosquito, new VoidInfection());
+                }
             }
         }
 

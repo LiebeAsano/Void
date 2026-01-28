e5e77ce395f3966f3487f588a23e10e726ce20d1
NV <ddd1020018@gmail.com>
2026-01-28 12:24:51 +0300

    CustomSleepMusic potential fix

--- src/MenuTinkery/CustomSleepMusic.cs
+++ src/MenuTinkery/CustomSleepMusic.cs
@@ -1,7 +1,5 @@
-﻿using Menu;
-using On.Menu;
-using System;
-using VoidTemplate.PlayerMechanics.Karma11Features;
+﻿using VoidTemplate.PlayerMechanics.Karma11Features;
+using KarmaLadderScreen = Menu.KarmaLadderScreen;
 
 namespace VoidTemplate.MenuTinkery;
 
@@ -9,38 +7,18 @@ public static class CustomSleepMusic
 {
     public static void Hook()
     {
-        On.Menu.SleepAndDeathScreen.Update += SleepAndDeathScreen_Update;
-        On.Menu.SleepAndDeathScreen.Singal += SleepAndDeathScreen_Singal;
+        On.Menu.SleepAndDeathScreen.GetDataFromGame += SleepAndDeathScreenOnGetDataFromGame;
     }
 
-    private static void SleepAndDeathScreen_Update(On.Menu.SleepAndDeathScreen.orig_Update orig, Menu.SleepAndDeathScreen self)
+    static void SleepAndDeathScreenOnGetDataFromGame(On.Menu.SleepAndDeathScreen.orig_GetDataFromGame orig, Menu.SleepAndDeathScreen self, KarmaLadderScreen.SleepDeathScreenDataPackage package)
     {
-        orig(self);
-
-        if (self?.saveState == null) return;
-
-        if (self.saveState.saveStateNumber == VoidEnums.SlugcatID.Void)
+        orig(self, package);
+        SaveState save = package.saveState;
+        if (save.saveStateNumber == VoidEnums.SlugcatID.Void 
+            && self.IsSleepScreen 
+            && (save.GetVoidMarkV3() || Karma11Update.VoidKarma11 || Karma11Update.VoidNightmare))
         {
-            if (self.IsSleepScreen && (self.saveState.GetVoidMarkV3() || Karma11Update.VoidKarma11 || Karma11Update.VoidNightmare) && self.soundLoop != null && self.mySoundLoopID != VoidEnums.SoundID.SleepMarkSound)
-            {
-                self.soundLoop.Destroy();
-                self.mySoundLoopID = VoidEnums.SoundID.SleepMarkSound;
-                self.PlaySound(self.mySoundLoopID);
-            }
-        }
-    }
-
-    private static void SleepAndDeathScreen_Singal(On.Menu.SleepAndDeathScreen.orig_Singal orig, Menu.SleepAndDeathScreen self, Menu.MenuObject sender, string message)
-    {
-        orig(self, sender, message);
-
-        if ((message == "CONTINUE" || message == "EXIT") && self.saveState?.saveStateNumber == VoidEnums.SlugcatID.Void)
-        {
-            if (self.soundLoop != null && self.mySoundLoopID == VoidEnums.SoundID.SleepMarkSound)
-            {
-                self.soundLoop.Destroy();
-                self.soundLoop = null;
-            }
+            self.mySoundLoopID = VoidEnums.SoundID.SleepMarkSound;
         }
     }
 }

74206ed75425b5ca73989b33859a25fac8693551
Liebe Asano <liebeasano@gmail.com>
2026-03-23 00:49:04 +0300

    Update ImmuneToFallDamage.cs

--- src/PlayerMechanics/ImmuneToFallDamage.cs
+++ src/PlayerMechanics/ImmuneToFallDamage.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RWCustom;
+using System;
 using VoidTemplate.Useful;
 
 namespace VoidTemplate.PlayerMechanics;
@@ -11,30 +8,38 @@ public static class ImmuneToFallDamage
 {
     public static void Hook()
     {
-        On.Player.UpdateMSC += Player_Update;
+        On.Player.TerrainImpact += Player_TerrainImpact;
     }
 
-    private static void Player_Update(On.Player.orig_UpdateMSC orig, Player self)
+    private static void Player_TerrainImpact(On.Player.orig_TerrainImpact orig, Player self, int chunk, IntVector2 direction, float speed, bool firstContact)
     {
-        orig(self);
-        if (self.AreVoidViy())
+        if (self.AreVoidViy() && !HasTempleGuard(self.room))
         {
-            bool hasGuardian = false;
-            if (self.room != null)
-            {
-                foreach (AbstractCreature creature in self.room.abstractRoom.creatures)
-                {
-                    if (creature.creatureTemplate.type == CreatureTemplate.Type.TempleGuard)
-                    {
-                        hasGuardian = true;
-                        break;
-                    }
-                }
-            }
-            if (!hasGuardian)
-            {
-                self.immuneToFallDamage = 1;
-            }
+            int old = self.immuneToFallDamage;
+            self.immuneToFallDamage = Math.Max(old, 1);
+
+            orig(self, chunk, direction, speed, firstContact);
+
+            self.immuneToFallDamage = old;
+            return;
         }
+
+        orig(self, chunk, direction, speed, firstContact);
+    }
+
+    private static bool HasTempleGuard(Room room)
+    {
+        if (room?.abstractRoom?.creatures == null)
+            return false;
+
+        var creatures = room.abstractRoom.creatures;
+        for (int i = 0; i < creatures.Count; i++)
+        {
+            var creature = creatures[i];
+            if (creature?.creatureTemplate?.type == CreatureTemplate.Type.TempleGuard)
+                return true;
+        }
+
+        return false;
     }
 }

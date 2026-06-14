28d2f12c3d84045d49262e7eb43920101e40bfff
Quansly <168752094+Quansly@users.noreply.github.com>
2026-06-14 19:06:20 +0700

    Light button

--- src/MenuTinkery/LastWishDiscordButton.cs
+++ src/MenuTinkery/LastWishDiscordButton.cs
@@ -1,20 +1,40 @@
 ﻿using System.Diagnostics;
 using Menu;
 using UnityEngine;
+using static Menu.Menu;
 
 namespace VoidTemplate.MenuTinkery
 {
-    public class LastWishDiscordButton : SymbolButton
+    public class LastWishDiscordButton : SimpleButton
     {
-        public LastWishDiscordButton(Menu.Menu menu, MenuObject owner, Vector2 pos) : base(menu, owner, symbolName: "atlas-void/discord_icon", "", pos)
+        public FSprite symbolSprite;
+
+        public LastWishDiscordButton(Menu.Menu menu, MenuObject owner, Vector2 pos) : base(menu, owner, "", "", pos, new(50, 50))
         {
-            roundedRect.size = new(50, 50);
-            size = new(50, 50);
+            symbolSprite = new("atlas-void/discord_icon");
+            Container.AddChild(symbolSprite);
         }
         public override void Clicked()
         {
             menu.PlaySound(SoundID.MENU_Switch_Page_In);
             Process.Start(new ProcessStartInfo("https://discord.gg/rainworldlastwish") { UseShellExecute = true });
+        }
+
+        public override void GrafUpdate(float timeStacker)
+        {
+            base.GrafUpdate(timeStacker);
+            var pos = DrawPos(timeStacker);
+            var size = DrawSize(timeStacker);
+            symbolSprite.SetPosition(pos + (size / 2));
+            float num = 0.5f - 0.5f * Mathf.Sin(Mathf.Lerp(buttonBehav.lastSin, buttonBehav.sin, timeStacker) / 30f * 3.1415927f * 2f);
+            num *= buttonBehav.sizeBump;
+            symbolSprite.color = (buttonBehav.greyedOut ? MenuRGB(MenuColors.VeryDarkGrey) : Color.Lerp(MyColor(timeStacker), MenuRGB(MenuColors.VeryDarkGrey), num));
+        }
+
+        public override void RemoveSprites()
+        {
+            symbolSprite.RemoveFromContainer();
+            base.RemoveSprites();
         }
     }
 }

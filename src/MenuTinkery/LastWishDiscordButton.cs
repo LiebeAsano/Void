b2a440176ca2817af8a11c96636754ac6cb75aa5
Quansly <168752094+Quansly@users.noreply.github.com>
2026-06-14 14:40:09 +0700

    Last Wish Discord Button

---
+++ src/MenuTinkery/LastWishDiscordButton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Menu;
+using UnityEngine;
+
+namespace VoidTemplate.MenuTinkery
+{
+    public class LastWishDiscordButton : SymbolButton
+    {
+        public LastWishDiscordButton(Menu.Menu menu, MenuObject owner, Vector2 pos) : base(menu, owner, symbolName: "pixel", "", pos)
+        {
+            roundedRect.size = new(64, 64);
+            size = new(64, 64);
+        }
+        public override void Clicked()
+        {
+            menu.PlaySound(SoundID.MENU_Switch_Page_In);
+            Process.Start(new ProcessStartInfo("https://discord.gg/rainworldlastwish") { UseShellExecute = true });
+        }
+    }
+}

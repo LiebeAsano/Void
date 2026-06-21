6c9ae0a607da0e99c960d830bfca18b8a2b2444d
Quansly <168752094+Quansly@users.noreply.github.com>
2026-06-21 15:26:14 +0700

    MS ghost put void to bed ZZZ

--- src/PortalForViy/MSGhostHooks.cs
+++ src/PortalForViy/MSGhostHooks.cs
@@ -15,11 +15,24 @@ namespace VoidTemplate.PortalForViy
         public static void Hook()
         {
             On.Ghost.FadeOutFinished += Ghost_FadeOutFinished;
+            On.Ghost.Update += Ghost_Update;
+        }
+
+        private static void Ghost_Update(On.Ghost.orig_Update orig, Ghost self, bool eu)
+        {
+            if (self.worldGhost.ghostID == MoreSlugcatsEnums.GhostID.MS && self.room.game.IsVoidStoryCampaign() && self.room.game.GetStorySession.saveState.deathPersistentSaveData.karmaCap < 9 && !self.hasRequestedShutDown && self.room.BeingViewed)
+            {
+                self.hasRequestedShutDown = true;
+                self.room.game.GetStorySession.saveState.sessionEndingFromSpinningTopEncounter = true;
+                self.room.game.Win(false, false);
+                RainWorldGame.ForceSaveNewDenLocation(self.room.game, "MS_S07", false);
+            }
+            orig(self, eu);
         }
 
         private static void Ghost_FadeOutFinished(On.Ghost.orig_FadeOutFinished orig, Ghost self)
         {
-            if (self.worldGhost.ghostID == MoreSlugcatsEnums.GhostID.MS && self.room.game.IsVoidWorld())
+            if (self.worldGhost.ghostID == MoreSlugcatsEnums.GhostID.MS && self.room.game.IsVoidStoryCampaign())
             {
                 var warp = new PlacedObject(PlacedObject.Type.WarpPoint, null)
                 {
@@ -28,7 +41,7 @@ namespace VoidTemplate.PortalForViy
                 var data = warp.data as WarpPoint.WarpPointData;
                 data.rippleWarp = true;
                 data.accessibility = WarpPoint.WarpPointData.WarpPointSpawnCondition.AnySlugcat;
-                var warpID = WarpPoint.IdentifyingString(self.room.game, warp.data as WarpPoint.WarpPointData, self.room.abstractRoom);
+                var warpID = WarpPoint.IdentifyingString(self.room.game, data, self.room.abstractRoom);
                 self.room.game.GetStorySession.saveState.deathPersistentSaveData.spawnedWarpPoints.Add(warpID, warp.ToString());
                 self.hasRequestedShutDown = true;
                 self.room.game.sawAGhost = self.worldGhost.ghostID;

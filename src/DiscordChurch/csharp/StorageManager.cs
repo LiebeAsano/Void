4793af09782204ad17e3d2a9dcb52d3f139c44cd
Liebe Asano <liebeasano@gmail.com>
2026-06-28 19:34:36 +0300

    Update StorageManager.cs

--- src/DiscordChurch/csharp/StorageManager.cs
+++ src/DiscordChurch/csharp/StorageManager.cs
@@ -1,20 +1,17 @@
-using System;
 using System.Collections.Generic;
-using System.Text;
 
-namespace Discord
+namespace Discord;
+
+public partial class StorageManager
 {
-    public partial class StorageManager
+    public IEnumerable<FileStat> Files()
     {
-        public IEnumerable<FileStat> Files()
+        var fileCount = Count();
+        var files = new List<FileStat>();
+        for (var i = 0; i < fileCount; i++)
         {
-            var fileCount = Count();
-            var files = new List<FileStat>();
-            for (var i = 0; i < fileCount; i++)
-            {
-                files.Add(StatAt(i));
-            }
-            return files;
+            files.Add(StatAt(i));
         }
+        return files;
     }
 }

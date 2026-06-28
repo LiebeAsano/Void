14d4ec19363592b208457d83485a4340ec5c72f0
Liebe Asano <liebeasano@gmail.com>
2026-06-28 19:35:04 +0300

    Update Expedition.cs

--- src/Misc/Expedition.cs
+++ src/Misc/Expedition.cs
@@ -1,22 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Expedition;
+using MoreSlugcats;
+using UnityEngine;
 
-namespace VoidTemplate.Misc
+namespace VoidTemplate.Misc;
+
+public static class Expedition
 {
-    public static class Expedition
+    public static void Hook()
     {
-        public static void Hook()
-        {
-            On.Expedition.AchievementChallenge.ValidForThisSlugcat += AchievementChallenge_ValidForThisSlugcat;
-        }
+        On.Expedition.VistaChallenge.ModifyVistaCandidates += VistaChallenge_ModifyVistaCandidates;
+        //On.Expedition.AchievementChallenge.ValidForThisSlugcat += AchievementChallenge_ValidForThisSlugcat;
+    }
 
-        private static bool AchievementChallenge_ValidForThisSlugcat(On.Expedition.AchievementChallenge.orig_ValidForThisSlugcat orig, global::Expedition.AchievementChallenge self, SlugcatStats.Name slugcat)
+    private static void VistaChallenge_ModifyVistaCandidates(On.Expedition.VistaChallenge.orig_ModifyVistaCandidates orig, VistaChallenge self, VistaChallenge input)
+    {
+        if (input.room == "HI_B04" && ModManager.MSC && ExpeditionData.slugcatPlayer != MoreSlugcatsEnums.SlugcatStatsName.Saint)
         {
-            if (slugcat == VoidEnums.SlugcatID.Void && self.ID == MoreSlugcats.MoreSlugcatsEnums.EndgameID.Martyr) return false;
-            return orig(self, slugcat);
+            input.location = new Vector2(2008f, 1385f);
+            return;
         }
+        orig(self, input);
+    }
+
+    private static bool AchievementChallenge_ValidForThisSlugcat(On.Expedition.AchievementChallenge.orig_ValidForThisSlugcat orig, global::Expedition.AchievementChallenge self, SlugcatStats.Name slugcat)
+    {
+        if (slugcat == VoidEnums.SlugcatID.Void && self.ID == MoreSlugcats.MoreSlugcatsEnums.EndgameID.Martyr) return false;
+        return orig(self, slugcat);
     }
 }

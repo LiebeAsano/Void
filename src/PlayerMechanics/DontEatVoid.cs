85184e1341aaf69d366d2922fb41e42a993e5063
Liebe Asano <liebeasano@gmail.com>
2026-06-28 19:36:58 +0300

    Update DontEatVoid.cs

--- src/PlayerMechanics/DontEatVoid.cs
+++ src/PlayerMechanics/DontEatVoid.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using VoidTemplate.PlayerMechanics.Karma11Features;
 using VoidTemplate.Useful;
 
 namespace VoidTemplate.PlayerMechanics;
@@ -16,12 +16,12 @@ public static class DontEatVoid
         if (self.eatMeat != 50 || self.AreVoidViy()) return;
         foreach (var grasp in self.grasps)
         {
-            if (grasp?.grabbed is Player prey && prey.AreVoidViy())
+            if (grasp?.grabbed is Player prey && prey.IsVoid() && !Karma11Update.VoidKarma11)
             {
                 self.Die();
                 break;
             }
-            if (grasp?.grabbed is Player prey2 && prey2.GetPlayerExt().voidPoisonBody)
+            if (grasp?.grabbed is Player prey2 && (prey2.GetPlayerExt().voidPoisonBody || prey2.IsViy() || prey2.IsVoid() && Karma11Update.VoidKarma11))
             {
                 self.GetPlayerExt().voidPoisonBody = true;
                 break;
